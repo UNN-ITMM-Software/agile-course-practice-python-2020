13227a02cd09df503632564353f1842da35184d7
Anastasia Timonova <timonova.a@gmail.com>
2015-10-26 19:56:48 +0300

    code style

--- code/timonova-anastasia/agile_lab1/tests/tests_for_matrix.py
+++ code/timonova-anastasia/agile_lab1/tests/tests_for_matrix.py
@@ -20,7 +20,7 @@ class MatrixTests(unittest.TestCase):
 
     def test_delete_zero_row_and_zero_col_in_matrix(self):
         test_matrix = Matrix.make_from_list([[1, 3], [5, 7]])
-        det = Matrix.calculate_det(Matrix.delete_col_and_row(
+        det = Matrix.calculate_det(Matrix.del_col_and_row(
             test_matrix, 0, 0))
         self.assertEqual(det, 7)
 
@@ -29,15 +29,18 @@ class MatrixTests(unittest.TestCase):
         det = Matrix.calculate_det(test_matrix)
         self.assertEqual(det, 54)
 
-    def test_can_calculate_determinant_with_second_column_decomposition(self):
-        test_matrix = Matrix.make_from_list([[1, 0, -3], [0, 0, 2], [-1, -2, 0]])
-        det = Matrix.calculate_det(Matrix.delete_col_and_row(test_matrix, 0, 1))
+    def test_can_calculate_determinant_with_second_col_decompos(self):
+        test_matrix = Matrix.make_from_list([[1, 0, -3], [0, 0, 2],
+                                             [-1, -2, 0]])
+        det = Matrix.calculate_det(Matrix.del_col_and_row(test_matrix, 0, 1))
         self.assertEqual(det, 2)
 
     def test_can_calculate_determinant_for_matrix6x6(self):
         test_matrix = Matrix.make_from_list([[-1, 2, 3, 4, 5, 6],
-                                             [7, 0, 4, -1, 0, -3], [0, 5, -6, 0, 0, 3],
-                                             [1, 0, 3, 4, 5, 6], [0, 8, 0, 1, 2, 0],
+                                             [7, 0, 4, -1, 0, -3],
+                                             [0, 5, -6, 0, 0, 3],
+                                             [1, 0, 3, 4, 5, 6],
+                                             [0, 8, 0, 1, 2, 0],
                                              [-4, 0, 6, 0, 0, -5]])
         det = Matrix.calculate_det(test_matrix)
         self.assertEqual(det, -96)
