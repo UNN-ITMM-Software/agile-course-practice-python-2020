c5d2cce573f2004bf1553ec2eb2fb90eff77a375
Oleg Kainov <kainov.oleg@gmail.com>
2015-11-09 19:21:45 +0300

    Added initial button state

--- code/kainov-oleg/src/viewmodel/viewmodel.py
+++ code/kainov-oleg/src/viewmodel/viewmodel.py
@@ -8,6 +8,7 @@ class ViewModel:
         self.first_fraction = ''
         self.second_fraction = ''
         self.operation = '+'
+        self.button_convert_state = 'normal'
 
     def get_button_convert_state(self):
         return self.button_convert_state
