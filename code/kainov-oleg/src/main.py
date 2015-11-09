0a897cbaeaa0a1d29e6203c5b7bfbf6b94ccc534
Oleg Kainov <kainov.oleg@gmail.com>
2015-11-09 21:22:50 +0300

    Just as I promised, CLI-versioned View

--- code/kainov-oleg/src/main.py
+++ code/kainov-oleg/src/main.py
@@ -1,4 +1,22 @@
+import argparse
+
 from view import view
+from cliview import cli_view
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--cli", action="store_true")
+    group.add_argument("--gui", action="store_true")
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    view.View().mainloop()
+    args = parse_args()
+    if args.cli:
+        cli_view.CLIView().mainloop()
+    elif args.gui:
+        view.View().mainloop()
+    else:
+        print 'Option is required'
