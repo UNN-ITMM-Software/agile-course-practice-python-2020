00f0e31d250a2fae174d5c326427db7a0b530265
Anastasia Timonova <timonova.a@gmail.com>
2015-12-06 17:48:46 +0300

    delete infrastruct..

--- code/timonova-anastasia/src/tests_for_txt_logger.py
+++ code/timonova-anastasia/src/tests_for_txt_logger.py
@@ -1,6 +1,7 @@
 import unittest
 
-from txt_logger import *
+from txt_logger import TxtLogger
+from txt_logger import LoggerError
 
 
 class TestForTxtLogger(unittest.TestCase):
@@ -11,4 +12,3 @@ class TestForTxtLogger(unittest.TestCase):
 
     # def test_can_create_file_on_disk(self):
     #     with self.assertRaises(LoggerError):
-
