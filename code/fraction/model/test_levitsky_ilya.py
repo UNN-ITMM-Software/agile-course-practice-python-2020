0a3413532e2543f1b57a5b4f948b9b1f585dddbf
Trintal <iliyalev@yandex.ru>
2020-11-02 15:08:07 +0300

    Fixing issues

--- code/fraction/model/test_levitsky_ilya.py
+++ code/fraction/model/test_levitsky_ilya.py
@@ -1,14 +1,16 @@
 import unittest
-import Fraction
+from fraction.model.fraction import Fraction
+
 
 class TestFractionClass(unittest.TestCase):
-    def test_default_fraction_is_5_7(self):
-        frac = Fraction()
+    def test_can_create_5_7_fraction(self):
+        frac = Fraction(5, 7)
         self.assertTrue(frac.is_equal(5, 7))
 
+
 class TestFractionOperations(unittest.TestCase):
-    def test_multiply_fractions_by_one(self):
-        frac_1 = Fraction(1, 2)
-        frac_2 = Fraction(2, 2)
+    def test_multiply_fractions_1_3_3_1(self):
+        frac_1 = Fraction(1, 3)
+        frac_2 = Fraction(3, 1)
         result = frac_1 * frac_2
-        self.assertTrue(result.is_equal(1, 2))
+        self.assertTrue(result.is_equal(1, 1))
