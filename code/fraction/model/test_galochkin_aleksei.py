3cd23b965ec697a6fe14d894d60210ade73ca831
ZextarTFC <lexagal.opera@yandex.ru>
2020-11-04 15:50:36 +0300

    Added more unit tests

---
+++ code/fraction/model/test_galochkin_aleksei.py
@@ -0,0 +1,26 @@
+import unittest
+
+from fraction.model.fraction import Fraction, InvalidFractionError
+
+
+class TestFractionClass(unittest.TestCase):
+
+    def test_can_create_1_fraction_from_str(self):
+        frac = Fraction.from_string('1')
+        self.assertTrue(frac.is_equal(1, 1))
+
+
+class TestFractionOperations(unittest.TestCase):
+
+    def test_divide_two_fractions(self):
+        frac_1 = Fraction(2, 3)
+        frac_2 = Fraction(2, 3)
+        result = frac_1 / frac_2
+        self.assertTrue(result.is_equal(1, 1))
+
+    def test_sum_fraction_1_2_1_2(self):
+        frac_1 = Fraction(1, 2)
+        frac_2 = Fraction(1, 2)
+        result = frac_1 + frac_2
+        self.assertTrue(result.is_equal(1, 1))
+
