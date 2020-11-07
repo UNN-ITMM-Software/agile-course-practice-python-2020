d9aab12c258c7730331a9c84e33cdcde9e094f9b
xnd-r <al.romanov2012@yandex.ru>
2020-11-08 01:08:23 +0300

    Added capitalization and revenue

--- code/depo_calc/model/depo_calc.py
+++ code/depo_calc/model/depo_calc.py
@@ -8,6 +8,15 @@ class Depo_calc():
                 is_add_to_depo=False,
                 freq=4, # capitalization frequency
                 is_add_to_S0=False):
+        assert all([S0 > 0,
+                    T > 0,
+                    T0 >= 0,
+                    r > 0,
+                    r < 1,
+                    is_add_to_depo in [True, False],
+                    freq in [1, 2, 3, 4, 6, 12],
+                    is_add_to_S0 in [True, False]
+                    ])
         self.S0=S0       
         self.T=T
         self.T0=T0
@@ -16,3 +25,17 @@ class Depo_calc():
         self.freq=freq
         self.is_add_to_S0=is_add_to_S0
         self.income=0
+
+    def capitalization(self):
+        income = self.S0 * self.r / self.freq
+        self.income += income
+        if self.is_add_to_depo:
+            self.S0 += income
+
+    def replenishment(self):
+        pass
+
+    def revenue(self):
+        for i in range(self.freq):
+            self.capitalization()
+        return self.income
--- code/depo_calc/model/tests_for_depo_calc.py
+++ code/depo_calc/model/tests_for_depo_calc.py
@@ -6,3 +6,24 @@ class TestDepoCelcClass(unittest.TestCase):
     def test_can_create_object(self):
         calc = Depo_calc()
         self.assertTrue(isinstance(calc, Depo_calc))
+
+    def test_cant_create_invalid_object(self):
+        with self.assertRaises(AssertionError):
+            calc = Depo_calc(S0=-1, freq=9)
+
+    def test_capitaliation_1(self):
+        calc = Depo_calc(is_add_to_depo=True, freq=1)
+        calc.capitalization()
+        self.assertEqual(calc.income, 5)
+        self.assertEqual(calc.S0, 105)
+
+    def test_capitaliation_2(self):
+        calc = Depo_calc(freq=1)
+        calc.capitalization()
+        self.assertEqual(calc.income, 5)
+        self.assertEqual(calc.S0, 100)
+
+
+    def test_revenue_1(self):
+        calc = Depo_calc(freq=2, is_add_to_depo=True)
+        self.assertEqual(calc.revenue(), 5.0625)
