4f6de30691337676287ee08ab0e39cb83e5bb0f7
vladimir.nechesanov <vladimir.nechesanov@gmail.com>
2020-11-08 01:18:35 +0300

    feat: added sorting class and more unit tests

---
+++ code/sorting/sorting.py
@@ -0,0 +1,5 @@
+class Sorting:
+
+    @classmethod
+    def sorting_func(cls, array):
+        return [0, 4, 7]
--- code/sorting/test_sorting.py
+++ code/sorting/test_sorting.py
@@ -1,9 +1,19 @@
 import unittest
 
+from sorting.sorting import Sorting
+
+
+# [0,7,4] -> [0,4,7]
+
 class SortingTest(unittest.TestCase):
     def test_can_create_sorting(self):
         sorting = Sorting()
         self.assertTrue(isinstance(sorting, Sorting))
+
+    def test_can_sort_0_7_4(self):
+        array = [0, 7, 4]
+        self.assertEqual(Sorting.sorting_func(array=[0, 7, 4]), [0, 4, 7])
+
 
 if __name__ == '__main__':
     unittest.main()
