85b07cc2319488f39d0f3e5b915e927cee8b195b
Krasikova Ekaterina <katrinkrasikova@mail.ru>
2020-11-07 22:17:10 +0300

    Add support of insertion

--- code/krasikova_ekaterina_lab1/model/dheap.py
+++ code/krasikova_ekaterina_lab1/model/dheap.py
@@ -2,3 +2,18 @@ class DHeap:
     def __init__(self, d=2):
         self.d = d
         self.heap = []
+
+    def _parent(self, i):
+        if i > 0 and i < len(self.heap):
+            return (i - 1) // self.d
+        else:
+            return -1
+
+    def insert(self, w):
+        self.heap.append(w)
+        j1 = len(self.heap) - 1
+        j2 = self._parent(j1)
+        while j2 != -1 and self.heap[j2] > self.heap[j1]:
+            self.heap[j1], self.heap[j2] = self.heap[j2], self.heap[j1]
+            j1 = j2
+            j2 = self._parent(j1)
--- code/krasikova_ekaterina_lab1/model/test_dheap.py
+++ code/krasikova_ekaterina_lab1/model/test_dheap.py
@@ -3,6 +3,24 @@ import unittest
 from dheap import DHeap
 
 class TestDHeap(unittest.TestCase):
-    def test_can_create_dheap(self):
+    def test_can_create_empty_3heap(self):
         dheap = DHeap(3)
         self.assertTrue(isinstance(dheap, DHeap))
+    
+    def test_can_insert_1_element_into_empty_3heap(self):
+        dheap = DHeap(3)
+        dheap.insert(5)
+        self.assertEqual(dheap.heap, [5])
+
+    def test_can_insert_2_elements_into_empty_3heap(self):
+        dheap = DHeap(3)
+        dheap.insert(5)
+        dheap.insert(3)
+        self.assertEqual(dheap.heap, [3, 5])
+
+    def test_can_insert_3_elements_into_empty_3heap(self):
+        dheap = DHeap(3)
+        dheap.insert(5)
+        dheap.insert(3)
+        dheap.insert(1)
+        self.assertTrue(dheap.heap[0] <= dheap.heap[1] and dheap.heap[0] <= dheap.heap[2])
