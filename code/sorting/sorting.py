e634df07a6c0e62c82f0f011f478017828ad9ef3
vladimir.nechesanov <vladimir.nechesanov@gmail.com>
2020-11-08 02:04:52 +0300

    fix: fixed test_can_convert_str_to_int

--- code/sorting/sorting.py
+++ code/sorting/sorting.py
@@ -4,3 +4,11 @@ class Sorting:
 
     def sorting_func(self):
         return [0, 4, 7]
+
+    def convert_to_array_of_int(self):
+        array_of_int = []
+
+        for i in self.array:
+            array_of_int.append(int(i))
+
+        return array_of_int
