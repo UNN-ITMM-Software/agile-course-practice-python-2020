310a3954551f0a2a4e97e11fc7e5a1f9c1ee318e
xnd-r <al.romanov2012@yandex.ru>
2020-11-09 00:57:54 +0300

    Added replenishment functionality

--- code/depo_calc/model/depo_calc.py
+++ code/depo_calc/model/depo_calc.py
@@ -1,41 +1,60 @@
 
-class Depo_calc():
-    def __init__(self,
-                S0=100, # Start depo
-                T=1, # Depo time, years
-                T0=0, # or XX.XX.XXXX
-                r=0.05, # percent
+class DepoCalc():
+    def __init__(
+                self,
+                s0=100.,  # Start depo
+                t=1.,  # Depo time, years
+                r=0.05,  # interest rate
                 is_add_to_depo=False,
-                freq=4, # capitalization frequency
-                is_add_to_S0=False):
-        assert all([S0 > 0,
-                    T > 0,
-                    T0 >= 0,
+                capitalization_freq=4,  # capitalization frequency
+                replenishment_freq=-1):
+        assert all([s0 > 0,
+                    t > 0,
                     r > 0,
                     r < 1,
                     is_add_to_depo in [True, False],
-                    freq in [1, 2, 3, 4, 6, 12],
-                    is_add_to_S0 in [True, False]
+                    capitalization_freq in [1, 2, 3, 4, 6, 12],
+                    replenishment_freq in [-1, 1, 2, 3, 4, 6, 12],
+                    t*12 >= capitalization_freq,
+                    capitalization_freq > replenishment_freq
                     ])
-        self.S0=S0       
-        self.T=T
-        self.T0=T0
-        self.r=r
-        self.is_add_to_depo=is_add_to_depo
-        self.freq=freq
-        self.is_add_to_S0=is_add_to_S0
-        self.income=0
+        self.s0 = s0
+        self.t = t
+        self.r = r
+        self.is_add_to_depo = is_add_to_depo
+        self.capitalization_freq = capitalization_freq
+        self.replenishment_freq = replenishment_freq
+        self.income = 0
+        self.repl = 0
 
     def capitalization(self):
-        income = self.S0 * self.r / self.freq
+        income = self.s0 * self.r / self.capitalization_freq
         self.income += income
         if self.is_add_to_depo:
-            self.S0 += income
+            self.s0 += income
 
-    def replenishment(self):
-        pass
+    def replenishment(self, value):
+        assert value > 0
+        self.s0 += value
+        self.repl += value
 
-    def revenue(self):
-        for i in range(self.freq):
-            self.capitalization()
-        return self.income
+    def revenue(self, values=None):
+        if self.replenishment_freq != -1:
+            assert self.capitalization_freq % self.replenishment_freq == 0
+        if self.replenishment_freq != -1:
+            assert len(values) == self.replenishment_freq * self.t
+            for i in range(int(self.capitalization_freq * self.t)):
+                j = 0
+                if (i+1) % (self.capitalization_freq / self.replenishment_freq) == 0:
+                    self.capitalization()
+                    self.replenishment(values[j])
+                    j += 1
+                else:
+                    self.capitalization()
+        else:
+            for i in range(self.capitalization_freq):
+                self.capitalization()
+        return int(self.income)
+
+    def get_data(self):
+        return self.s0, self.repl, self.income
--- code/depo_calc/model/tests_for_depo_calc.py
+++ code/depo_calc/model/tests_for_depo_calc.py
@@ -1,29 +1,65 @@
 import unittest
 
-from depo_calc.model.depo_calc import Depo_calc
+from depo_calc.model.depo_calc import DepoCalc
+
 
 class TestDepoCelcClass(unittest.TestCase):
     def test_can_create_object(self):
-        calc = Depo_calc()
-        self.assertTrue(isinstance(calc, Depo_calc))
+        calc = DepoCalc()
+        self.assertTrue(isinstance(calc, DepoCalc))
 
-    def test_cant_create_invalid_object(self):
+    def test_cant_create_invalid_object_1(self):
         with self.assertRaises(AssertionError):
-            calc = Depo_calc(S0=-1, freq=9)
+            DepoCalc(s0=-1, capitalization_freq=9)
 
     def test_capitaliation_1(self):
-        calc = Depo_calc(is_add_to_depo=True, freq=1)
+        calc = DepoCalc(is_add_to_depo=True, capitalization_freq=1)
         calc.capitalization()
         self.assertEqual(calc.income, 5)
-        self.assertEqual(calc.S0, 105)
+        self.assertEqual(calc.s0, 105)
 
     def test_capitaliation_2(self):
-        calc = Depo_calc(freq=1)
+        calc = DepoCalc(capitalization_freq=1)
         calc.capitalization()
         self.assertEqual(calc.income, 5)
-        self.assertEqual(calc.S0, 100)
+        self.assertEqual(calc.s0, 100)
 
+    def test_can_replenish(self):
+        calc = DepoCalc(capitalization_freq=4, replenishment_freq=2)
+        calc.replenishment(10)
+        calc.replenishment(20)
+        self.assertEqual(calc.s0, 130)
+
+    def test_cant_replenish_negative_value(self):
+        calc = DepoCalc(capitalization_freq=2, replenishment_freq=1)
+        with self.assertRaises(AssertionError):
+            calc.replenishment(-10)
 
     def test_revenue_1(self):
-        calc = Depo_calc(freq=2, is_add_to_depo=True)
-        self.assertEqual(calc.revenue(), 5.0625)
+        calc = DepoCalc(capitalization_freq=2, is_add_to_depo=True)
+        self.assertEqual(calc.revenue(), 5)
+
+    def test_revenue_2(self):
+        calc = DepoCalc(s0=100000, capitalization_freq=4, replenishment_freq=2, is_add_to_depo=True)
+        self.assertEqual(calc.revenue(values=[10000, 10000]), 5346)
+
+    def test_revenue_3(self):
+        calc = DepoCalc(t=1.5, s0=100000, capitalization_freq=4, replenishment_freq=2, is_add_to_depo=True)
+        self.assertEqual(calc.revenue(values=[10000, 10000, 10000]), 8499)
+
+    def test_revenue_no_add_to_depo(self):
+        calc = DepoCalc(s0=100000, capitalization_freq=4, replenishment_freq=2, is_add_to_depo=False)
+        calc.revenue(values=[10000, 10000])
+        self.assertEqual(calc.income, 5250)
+
+    def test_cant_create_invalid_object_2(self):
+        with self.assertRaises(AssertionError):
+            DepoCalc(capitalization_freq=1, replenishment_freq=2)
+
+    def test_can_print_data(self):
+        calc = DepoCalc(s0=10000, capitalization_freq=1)
+        calc.revenue()
+        s, r, i = calc.get_data()
+        self.assertEqual(s, 10000)
+        self.assertEqual(r, 0)
+        self.assertEqual(i, 500)
