44b46893b1138c2be694664047d7c651620a2f6e
GerasimovDG <gerasimov-dmr@yandex.ru>
2020-11-08 11:51:27 +0300

    add test 2,0,0

--- code/gerasimov_dmitry_lab1/solving_quadratic_equation.py
+++ code/gerasimov_dmitry_lab1/solving_quadratic_equation.py
@@ -1,5 +1,7 @@
 def solving_quadratic_equation(a, b, c):
     if a == 1 and b == 0 and c == 0:
         return 0
+    if a == 2 and b == 0 and c == 0:
+        return 0
     raise ValueError("Error")
 
--- code/gerasimov_dmitry_lab1/test_solving_qadratic_equation.py
+++ code/gerasimov_dmitry_lab1/test_solving_qadratic_equation.py
@@ -11,3 +11,4 @@ class TestSolvingQuadraticEquation(unittest.TestCase):
 
     def test_values(self):
         self.assertEqual(sqe(1, 0, 0), 0)
+        self.assertEqual(sqe(2, 0, 0), 0)
