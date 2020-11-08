2f9a7661d1218e91cb5f18e35009cc48f2b7663b
Zinoviev, Vladimir <vladimir.zinoviev@intel.com>
2020-11-09 02:41:38 +0300

    code coverage fix by changing test example

--- code/fraction/model/test_VladimirZinoviev.py
+++ code/fraction/model/test_VladimirZinoviev.py
@@ -4,11 +4,9 @@ from fraction.model.fraction import Fraction
 
 
 class TestFractionClass(unittest.TestCase):
-    def test_create_fraction(self):
-        try:
-            Fraction()
-        except Exception:
-            self.fail('Can not create Fraction')
+    def test_is_not_fraction_a(self):
+        result = Fraction.is_fraction('1/b')
+        self.assertFalse(result)
 
     def test_that_2_4_is_equal_1_2(self):
         frac = Fraction(2, 4)
