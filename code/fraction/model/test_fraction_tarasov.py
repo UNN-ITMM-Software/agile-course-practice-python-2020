1394db244bd98c841660d071ef7bc50ba36a5ab5
Splav555 <olegka35@gmail.com>
2020-11-08 22:26:18 +0300

    Added more unit tests

--- code/fraction/model/test_fraction_tarasov.py
+++ code/fraction/model/test_fraction_tarasov.py
@@ -6,8 +6,8 @@ from fraction.model.fraction import Fraction
 class FractionTestClass(unittest.TestCase):
 
     def test_fraction_init(self):
-        fraction = Fraction(5, 10)
-        self.assertTrue(fraction.is_equal(5, 10))
+        fraction = Fraction(5, 9)
+        self.assertTrue(fraction.is_equal(5, 9))
 
     def test_auto_reducing(self):
         fraction = Fraction(2, 4)
