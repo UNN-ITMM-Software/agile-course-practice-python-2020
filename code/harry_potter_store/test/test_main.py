d1d5db3b75f6bb8fbfd6e92cc9f89d47ddcfd371
Daniil Roman <d.roman@just-ai.com>
2020-11-08 14:05:57 +0300

    [lab1] init module and Store class

---
+++ code/harry_potter_store/test/test_main.py
@@ -0,0 +1,11 @@
+import unittest
+
+from harry_potter_store.src.store import Store
+
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        pass
+
+    def test_can_create_store(self):
+        store = Store()
