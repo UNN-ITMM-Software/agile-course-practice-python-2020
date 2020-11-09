596b1374abcf90e0a5386f6ac308ec6db85e017b
Daniil Roman <d.roman@just-ai.com>
2020-11-09 23:30:29 +0300

    [lab1] fix PR review remarks

--- code/harry_potter_store/src/store.py
+++ code/harry_potter_store/src/store.py
@@ -1,3 +1,7 @@
+from copy import deepcopy
+from typing import Dict
+
+
 class Store:
     book_types = ["1", "2", "3", "4", "5"]
 
@@ -5,29 +9,67 @@ class Store:
         self.book_price = book_price
         pass
 
-    def _clean(self, to_buy_books: dict) -> dict:
+    def _validate(self, to_buy_books: Dict[str, int]):
+        for book_type, amount in to_buy_books.items():
+            if book_type not in Store.book_types:
+                raise ValueError(f"Book type {book_type} must be in {Store.book_types}")
+            if amount <= 0:
+                raise ValueError(f"Book amount {amount} for type {book_type} must be > 0")
+
+    def _clean(self, to_buy_books: Dict[str, int]) -> dict:
         return dict(filter(lambda item: item[1] > 0 and item[0] in Store.book_types, to_buy_books.items()))
 
-    def _decrement(self, to_buy_books: dict) -> dict:
-        return self._clean(dict(map(lambda i: [i[0], i[1] - 1], to_buy_books.items())))
+    def _decrement(self, to_buy_books: Dict[str, int], count: int) -> dict:
+        first_part = {book: amount - 1 for book, amount in list(to_buy_books.items())[:count]}
+        second_part = dict(list(to_buy_books.items())[count:])
+        first_part.update(second_part)
+        return self._clean(first_part)
 
-    # book_types = {"str_type": int_count}
-    def get_price(self, to_buy_books: dict) -> float:
+    def _discount_2(self, to_buy_books, price, predicate):
+        if predicate(len(to_buy_books), 2):
+            price += 2 * self.book_price * 0.95
+            to_buy_books = self._decrement(to_buy_books, 2)
+        return to_buy_books, price
+
+    def _discount_3(self, to_buy_books, price, predicate):
+        if predicate(len(to_buy_books), 3):
+            price += 3 * self.book_price * 0.90
+            to_buy_books = self._decrement(to_buy_books, 3)
+        return to_buy_books, price
+
+    def _discount_4(self, to_buy_books, price, predicate):
+        if predicate(len(to_buy_books), 4):
+            price += 4 * self.book_price * 0.80
+            to_buy_books = self._decrement(to_buy_books, 4)
+        return to_buy_books, price
+
+    def _discount_5(self, to_buy_books, price, predicate):
+        if predicate(len(to_buy_books), 5):
+            price += 5 * self.book_price * 0.75
+            to_buy_books = self._decrement(to_buy_books, 5)
+        return to_buy_books, price
+
+    """
+    to_buy_books - dictionary from book type and book amount
+    key of dict -- book type
+    value of dict -- book amount  
+    """
+
+    def get_price(self, to_buy_books: Dict[str, int]) -> float:
+        self._validate(to_buy_books)
+        discounts = [self._discount_5, self._discount_4, self._discount_3, self._discount_2]
+        prices = []
+        for i in range(len(discounts)):
+            prices.append(self._get_price(to_buy_books, [discounts[i]], len(discounts) - i, lambda x, y: x >= y))
+            prices.append(self._get_price(to_buy_books, discounts[0:i + 1], len(discounts) - i, lambda x, y: x == y))
+        return min(prices)
+
+    def _get_price(self, to_buy_books: Dict[str, int], discount_funcs: list, min_value: int, predicate) -> float:
+        to_buy_books = deepcopy(to_buy_books)
         price = 0
-        to_buy_books = self._clean(to_buy_books)
-        while len(to_buy_books) > 1:
-            if len(to_buy_books) == 5:
-                price += 5*self.book_price * 0.75
-                to_buy_books = self._decrement(to_buy_books)
-            if len(to_buy_books) == 4:
-                price += 4*self.book_price * 0.80
-                to_buy_books = self._decrement(to_buy_books)
-            if len(to_buy_books) == 3:
-                price += 3*self.book_price * 0.90
-                to_buy_books = self._decrement(to_buy_books)
-            if len(to_buy_books) == 2:
-                price += 2*self.book_price * 0.95
-                to_buy_books = self._decrement(to_buy_books)
+        while len(to_buy_books) > min_value:
+            for discount_func in discount_funcs:
+                to_buy_books, price = discount_func(to_buy_books, price, predicate)
 
         count = sum(to_buy_books.values())
         return price + self.book_price * count
--- code/harry_potter_store/test/test_store.py
+++ code/harry_potter_store/test/test_store.py
@@ -4,72 +4,58 @@ from harry_potter_store.src.store import Store
 
 
 class TestMain(unittest.TestCase):
-
-    def test_can_create_store(self):
-        Store()
+    @classmethod
+    def setUpClass(self):
+        self.store = Store()
 
     def test_store_contain_five_book_types(self):
-        self.assertEqual(len(Store.book_types), 5)
+        self.assertEqual(5, len(Store.book_types))
 
     def test_get_price_1(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 1}), 8)
+        self.assertEqual(8, self.store.get_price({"1": 1}))
 
     def test_get_price_2(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 2}), 16)
+        self.assertEqual(16, self.store.get_price({"1": 2}))
 
     def test_get_price_with_discount_2_books(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 1, "2": 1}), 16 * 0.95)
+        self.assertEqual(16 * 0.95, self.store.get_price({"1": 1, "2": 1}))
 
     def test_get_price_with_discount_3_books(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 1, "2": 1, "3": 1}), 24 * 0.9)
+        self.assertEqual(24 * 0.9, self.store.get_price({"1": 1, "2": 1, "3": 1}))
 
     def test_get_price_with_discount_4_books(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 1, "2": 1, "3": 1, "4": 1}), 32 * 0.8)
+        self.assertEqual(32 * 0.8, self.store.get_price({"1": 1, "2": 1, "3": 1, "4": 1}))
 
     def test_get_price_with_discount_5_books(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 1, "2": 1, "3": 1, "4": 1, "5": 1}), 40 * 0.75)
+        self.assertEqual(40 * 0.75, self.store.get_price({"1": 1, "2": 1, "3": 1, "4": 1, "5": 1}))
 
     def test_get_price_complex_1(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 1, "2": 2}), 16 * 0.95 + 8)
+        self.assertEqual(16 * 0.95 + 8, self.store.get_price({"1": 1, "2": 2}))
 
     def test_get_price_complex_2(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 0, "2": 2}), 16)
+        self.assertRaises(ValueError, lambda: self.store.get_price({"1": 0, "2": 2}))
 
     def test_get_price_complex_3(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": -132, "2": -3324}), 0)
+        self.assertRaises(ValueError, lambda: self.store.get_price({"1": -132, "2": -3324}))
 
     def test_get_price_complex_4(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": -132, "2": -3324, "111": 333}), 0)
+        self.assertRaises(ValueError, lambda: self.store.get_price({"1": -132, "2": -3324, "111": 333}))
 
     def test_get_price_complex_5(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 3, "2": 1, "3": 2}), 24 * 0.90 + 16 * 0.95 + 8)
+        self.assertEqual(24 * 0.90 + 16 * 0.95 + 8, self.store.get_price({"1": 3, "2": 1, "3": 2}))
 
     def test_get_price_complex_6(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 2, "2": 2, "3": 2, "4": 2}), (32 * 0.8) * 2)
+        self.assertEqual((32 * 0.8) * 2, self.store.get_price({"1": 2, "2": 2, "3": 2, "4": 2}))
 
     def test_get_price_complex_7(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 2, "2": 2, "3": 2, "4": 1, "5": 1}),
-                         (40 * 0.75) + (24 * 0.9))
+        self.assertEqual(32 * 0.8 * 2,
+                         self.store.get_price({"1": 2, "2": 2, "3": 2, "4": 1, "5": 1}))
 
     def test_get_price_complex_8(self):
-        store = Store()
-        self.assertEqual(store.get_price({"1": 2, "2": 2, "3": 2, "4": 3, "5": 4}),
-                         (40 * 0.75 * 2) + (16 * 0.95) + 8)
+        self.assertEqual((40 * 0.75 * 2) + (16 * 0.95) + 8,
+                         self.store.get_price({"1": 2, "2": 2, "3": 2, "4": 3, "5": 4}))
 
     def test_another_book_price(self):
         store = Store(10)
-        self.assertEqual(store.get_price({"1": 2, "2": 2, "3": 2, "4": 3, "5": 4}),
-                         (50 * 0.75 * 2) + (20 * 0.95) + 10)
+        self.assertEqual((50 * 0.75 * 2) + (20 * 0.95) + 10,
+                         store.get_price({"1": 2, "2": 2, "3": 2, "4": 3, "5": 4}))
