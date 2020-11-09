30b3f7b00b11dd4e025c5b3f3d061a934a5ab050
vladimir.nechesanov <vladimir.nechesanov@gmail.com>
2020-11-09 23:04:38 +0300

    fix: fixed comments

--- code/sorting/sorting.py
+++ code/sorting/sorting.py
@@ -1,21 +1,19 @@
-stringType = type('')
-
 
 class Sorting:
     def __init__(self, array):
+        if not isinstance(array, list):
+            raise TypeError("Must be list")
+
         self.array = array
 
     def convert_to_array_of_int(self):
         array_of_int = []
 
         for i in self.array:
-            if type(i) == stringType and not i.isalpha():
+            if isinstance(i, str) and i.isnumeric():
                 array_of_int.append(int(i))
-            if type(i) == stringType and not i.isalpha():
-                continue
-            if type(i) != stringType:
+            elif not isinstance(i, str):
                 array_of_int.append(i)
-
         return array_of_int
 
     def insertion_sort(self):
@@ -26,7 +24,7 @@ class Sorting:
             curr_pos = index
             while curr_pos > 0 and nums[curr_pos - 1] > curr_val:
                 nums[curr_pos] = nums[curr_pos - 1]
-                curr_pos = curr_pos - 1
+                curr_pos -= 1
             nums[curr_pos] = curr_val
 
         return nums
--- code/sorting/test_sorting.py
+++ code/sorting/test_sorting.py
@@ -6,6 +6,9 @@ FIRST_CASE = [0, 7, 4]
 SECOND_CASE = [0, 45, 1,  8, 2, 4]
 THIRD_CASE = [0, 4, '45', '1', 2, 8]
 FOURTH_CASE = [1, '45', 3, 5, 'wef']
+FIFTH_CASE = [0, 5, -1, 56, -7, -44]
+SIXTH_CASE = [0.5, 1.5, 1.6, 0.05, 7.5, 3.4]
+SEVENTH_CASE = ['123abc', 5, 0, '45']
 
 
 class SortingTest(unittest.TestCase):
@@ -16,6 +19,10 @@ class SortingTest(unittest.TestCase):
     def test_init_param_list(self):
         arr = [1, 2, 3]
         self.assertEqual(arr, Sorting(arr).array)
+
+    def test_sorting_should_be_list(self):
+        arr = [3]
+        self.assertTrue(Sorting(arr))
 
     def test_can_sort_first_case(self):
         sort = Sorting(FIRST_CASE)
@@ -40,3 +47,15 @@ class SortingTest(unittest.TestCase):
     def test_can_sort_fourth_case(self):
         sort = Sorting(FOURTH_CASE)
         self.assertEqual(sort.insertion_sort(), [1, 3, 5, 45])
+
+    def test_can_sort_negtive_values(self):
+        sort = Sorting(FIFTH_CASE)
+        self.assertEqual(sort.insertion_sort(), [-44, -7, -1, 0, 5, 56])
+
+    def test_can_sort_float_values(self):
+        sort = Sorting(SIXTH_CASE)
+        self.assertEqual(sort.insertion_sort(), [0.05, 0.5, 1.5, 1.6, 3.4, 7.5])
+
+    def test_can_sort_seventh_case(self):
+        sort = Sorting(SEVENTH_CASE)
+        self.assertEqual(sort.insertion_sort(), [0, 5, 45])
