29691552f53a3d1ca2e99fec1ae68d08533e8d42
Kuznetsov, Victor <victor.kuznetsov@intel.com>
2020-11-09 22:09:43 +0300

    remove f strings

--- code/kuznetsov_victor_lab1/debt.py
+++ code/kuznetsov_victor_lab1/debt.py
@@ -5,13 +5,13 @@ class DebtExpenses(object):
     def __init__(self, req_sum: Union[int, float], percent_rate: Union[int, float], period: int):
         if req_sum < 0 or percent_rate < 0 or period < 0:
             raise ValueError('Values must be positive numbers!')
-        self.req_sum = req_sum  # D
-        self.percent_rate = percent_rate / 100  # g
-        self.period = period  # n
+        self.req_sum = req_sum
+        self.percent_rate = percent_rate / 100
+        self.period = period
 
     def equal_amounts_repayment(self, year: int):
         if year < 1 or year > self.period:
-            raise Exception(f'Year must be less than {self.period}!')
+            raise Exception('Year must be less than {}!'.format(self.period))
 
         expenses = round(self.req_sum / self.period)
         percent_for_year = (self.req_sum - expenses * (year - 1)) * self.percent_rate
@@ -20,7 +20,7 @@ class DebtExpenses(object):
 
     def equal_payments_repayment(self, year: int):
         if year < 1 or year > self.period:
-            raise Exception(f'Year must be less than {self.period}!')
+            raise Exception('Year must be less than {}!'.format(self.period))
 
         convertion_rate = (1 - (1 + self.percent_rate) ** (-self.period)) / self.percent_rate
         payment = round(self.req_sum / convertion_rate)
