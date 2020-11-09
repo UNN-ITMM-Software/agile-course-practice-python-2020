73f51837de47827d7e40b6895180be3758b6994c
PanovaElena <alyona-gra98@yandex.ru>
2020-11-10 00:51:03 +0300

    fix code style

--- code/fraction/model/test_panova_elena.py
+++ code/fraction/model/test_panova_elena.py
@@ -7,7 +7,7 @@ class TestFractionClass(unittest.TestCase):
     def test_can_create_str_from_fraction(self):
         frac = Fraction(p=3, q=4)
         self.assertEqual(str(frac), '3/4')
-        
+
     def test_can_reduce_fraction(self):
         frac = Fraction(p=5, q=10)
         self.assertTrue(frac.is_equal(1, 2))
