982e7ee4353135037ccfe3090bfd2d08d18bc68c
Aleksandr <aleksandr-generalov19@yandex.ru>
2020-11-10 23:12:45 +0300

    Fixing issues

--- code/fraction/model/test_Generalov.py
+++ code/fraction/model/test_Generalov.py
@@ -9,13 +9,13 @@ class TestFraction(unittest.TestCase):
         self.assertTrue(isinstance(frac, Fraction))
 
     def test_multiply_fraction(self):
-        frac_1 = Fraction(1, 3)
+        frac_1 = Fraction(1, 2)
         frac_2 = Fraction(3, 4)
         result = frac_1 * frac_2
         self.assertTrue(result.is_equal(3, 8))
 
-    def test_sum_fraction_1_3_3_4(self):
-        frac_1 = Fraction(1, 3)
+    def test_sum_fraction_1_2_3_4(self):
+        frac_1 = Fraction(1, 2)
         frac_2 = Fraction(3, 4)
         result = frac_1 + frac_2
         self.assertTrue(result.is_equal(5, 4))
