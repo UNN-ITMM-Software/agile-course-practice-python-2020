7a61402468f9617cd223db1036b78f33a90291b2
VlaKuz <vvk-97@mail.ru>
2020-11-10 16:46:43 +0300

    fix graph init and condition in algorithm

--- code/dijkstra_algorithm/model/dijkstra.py
+++ code/dijkstra_algorithm/model/dijkstra.py
@@ -2,21 +2,35 @@ import sys
 
 
 class Graph(object):
-    def __init__(self, vertices):
+    def __init__(self, vertices, graph):
         if not isinstance(vertices, int):
-            raise TypeError
-        if vertices < 0:
-            raise ValueError
+            raise TypeError('Incorrect type of input vertices. Expected: int')
+        if vertices <= 0:
+            raise ValueError('Unacceptable input value of vertices')
         self.vertices = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
+        if len(graph) != self.vertices:
+            raise ValueError('Wrong length in graphs row')
 
-    def minimal_distance(self, distances, found_vertex_distances):
+        for row in range(self.vertices):
+            if len(graph[row]) != self.vertices:
+                raise ValueError('Wrong length in graphs column')
+            for column in range(self.vertices):
+                if not isinstance(graph[row][column], int) and \
+                        not isinstance(graph[row][column], float):
+                    raise TypeError('Wrong type of value in graph matrix')
+
+        for row in range(0, len(graph) - 1):
+            for column in range(row + 1, len(graph[row])):
+                if graph[row][column] != graph[column][row]:
+                    raise ValueError('Matrix is not symmetric')
+        self.graph = graph
+
+    def minimal_distance(self, distances, visited_vertices):
         minimal = sys.maxsize
         minimal_index = 0
 
         for vertex in range(self.vertices):
-            if distances[vertex] < minimal and not found_vertex_distances[vertex]:
+            if distances[vertex] < minimal and vertex not in visited_vertices:
                 minimal = distances[vertex]
                 minimal_index = vertex
         return minimal_index
@@ -28,26 +42,16 @@ class Graph(object):
         if start_vertex < 0 or start_vertex >= self.vertices:
             raise ValueError('Wrong value of source vertex')
 
-        if len(self.graph) != self.vertices:
-            raise ValueError('Wrong length in graphs row')
-
-        for row in range(self.vertices):
-            if len(self.graph[row]) != self.vertices:
-                raise ValueError('Wrong length in graphs column')
-            for column in range(self.vertices):
-                if not isinstance(self.graph[row][column], int) and \
-                        not isinstance(self.graph[row][column], float):
-                    raise TypeError('Wrong type of value in graph matrix')
         distances = [sys.maxsize] * self.vertices
         distances[start_vertex] = 0
-        found_vertex_distances = [False] * self.vertices
+        visited_vertices = set()
 
         for count in range(self.vertices):
-            idx_current_min_vertex = self.minimal_distance(distances, found_vertex_distances)
-            found_vertex_distances[idx_current_min_vertex] = True
+            idx_current_min_vertex = self.minimal_distance(distances, visited_vertices)
+            visited_vertices.add(idx_current_min_vertex)
             for vertex in range(self.vertices):
-                if self.graph[idx_current_min_vertex][vertex] > 0 and \
-                        not found_vertex_distances[vertex] and \
+                if vertex not in visited_vertices and \
+                        self.graph[idx_current_min_vertex][vertex] > 0 and \
                         distances[vertex] > distances[idx_current_min_vertex] +\
                         self.graph[idx_current_min_vertex][vertex]:
                     distances[vertex] = distances[idx_current_min_vertex] + \
