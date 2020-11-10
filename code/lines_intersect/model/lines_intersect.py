533f8b218ea5c661976c41c8bcd45420ad0c5ebb
PanovaElena <alyona-gra98@yandex.ru>
2020-11-10 23:08:07 +0300

    bug fix

--- code/lines_intersect/model/lines_intersect.py
+++ code/lines_intersect/model/lines_intersect.py
@@ -19,22 +19,30 @@ def determine_if_two_line_segments_intersect(line_segment_1, line_segment_2):
     p21, p22 = line_segment_2
 
     inv_matrix = np.array([[p21[1]-p22[1], p22[0]-p21[0]],
-                           [p11[1]-p12[1], p12[0]-p11[0]]])
+                           [p11[1]-p12[1], p12[0]-p11[0]]], dtype=np.float64)
     det = np.linalg.det(inv_matrix)
 
-    if det == 0.0:  # line segments are parallel
-        if np.linalg.det([[p11[0]-p21[0], p11[0]-p12[0]],
-                          [p11[1]-p21[1], p11[1]-p12[1]]]) != 0.0:
+    def is_float_eq(a, b):
+        return np.isclose(a, b, rtol=1e-10, atol=1e-12)
+
+    def is_float_between(a, c1, c2):
+        return is_float_eq(a, c1) or is_float_eq(a, c2) or c1 < a < c2
+
+    if is_float_eq(det, 0.0):  # line segments are parallel
+        if not is_float_eq(np.linalg.det(np.array([[p11[0]-p21[0], p11[0]-p12[0]],
+                                                  [p11[1]-p21[1], p11[1]-p12[1]]],
+                                                  dtype=np.float64)), 0.0):
             return False
 
         def is_point_on_line(point, line):
-            return line[0][0] <= point[0] <= line[1][0] and line[0][1] <= point[1] <= line[1][1]
+            check_x = is_float_between(point[0], line[0][0], line[1][0])
+            check_y = is_float_between(point[1], line[0][1], line[1][1])
+            return check_x and check_y
 
         check_line1 = is_point_on_line(p11, line_segment_2) or is_point_on_line(p12, line_segment_2)
         check_line2 = is_point_on_line(p21, line_segment_1) or is_point_on_line(p22, line_segment_1)
         return check_line1 or check_line2
 
-    vector = np.array([p21[0]-p11[0], p21[1]-p11[1]])
+    vector = np.array([p21[0]-p11[0], p21[1]-p11[1]], dtype=np.float64)
     params = np.dot(inv_matrix, vector) / det
-    print(params)
-    return 0.0 <= params[0] <= 1.0 and 0.0 <= params[1] <= 1.0
+    return is_float_between(params[0], 0, 1) and is_float_between(params[1], 0, 1)
