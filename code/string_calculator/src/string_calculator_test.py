1443691a97a10cd4ba6cde9f400dca58e113825d
Pavel Baranov <pavelbaranow@gmail.com>
2020-11-10 23:53:30 +0300

    Replace statements assertTrue with assertEqual

--- code/string_calculator/src/string_calculator_test.py
+++ code/string_calculator/src/string_calculator_test.py
@@ -13,42 +13,42 @@ class TestStrCalculatorClass(unittest.TestCase):
     def test_add_empty_string_is_0(self):
         strcalc = StrCalculator()
         result = strcalc.add("")
-        self.assertTrue(result == 0)
+        self.assertEqual(result, 0)
 
     def test_add_string_1(self):
         strcalc = StrCalculator()
         result = strcalc.add("1")
-        self.assertTrue(result == 1)
+        self.assertEqual(result, 1)
 
     def test_add_strings_1_2(self):
         strcalc = StrCalculator()
         result = strcalc.add("1,2")
-        self.assertTrue(result == 3)
+        self.assertEqual(result, 3)
 
     def test_can_add_unknown_amount_strings(self):
         strcalc = StrCalculator()
         result = strcalc.add("2,3,1,0,5,6,0,1,2")
-        self.assertTrue(result == 20)
+        self.assertEqual(result, 20)
 
     def test_can_use_newline_delimiter(self):
         strcalc = StrCalculator()
         result = strcalc.add("1\n2,3")
-        self.assertTrue(result == 6)
+        self.assertEqual(result, 6)
 
     def test_can_use_newline_delimiter_mix(self):
         strcalc = StrCalculator()
         result = strcalc.add("1\n2,3,4\n5,6\n7")
-        self.assertTrue(result == 28)
+        self.assertEqual(result, 28)
 
     def test_can_use_custom_delimiter(self):
         strcalc = StrCalculator()
         result = strcalc.add(";\n1;2")
-        self.assertTrue(result == 3)
+        self.assertEqual(result, 3)
 
     def test_can_use_custom_delimiter_hard(self):
         strcalc = StrCalculator()
         result = strcalc.add(";\n1;2;3;4;5;6;7")
-        self.assertTrue(result == 28)
+        self.assertEqual(result, 28)
 
     def test_check_negative_numbers_exception(self):
         strcalc = StrCalculator()
@@ -71,39 +71,39 @@ class TestStrCalculatorParserClass(unittest.TestCase):
     def test_parse_check_string_with_default_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse("1,2").string
-        self.assertTrue(result == "1,2")
+        self.assertEqual(result, "1,2")
 
     def test_parse_check_delimiters_with_default_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse("1,2").delimiters
-        self.assertTrue(result == ["\n", ","])
+        self.assertEqual(result, ["\n", ","])
 
     def test_parse_check_numstring_with_default_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse("1,2").numstring
-        self.assertTrue(result == "1,2")
+        self.assertEqual(result, "1,2")
 
     def test_parse_check_numbers_with_default_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse("1,2").numbers
-        self.assertTrue(result == [1, 2])
+        self.assertEqual(result, [1, 2])
 
     def test_parse_check_string_with_custom_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse(";\n1;2").string
-        self.assertTrue(result == ";\n1;2")
+        self.assertEqual(result, ";\n1;2")
 
     def test_parse_check_delimiters_with_custom_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse(";\n1;2").delimiters
-        self.assertTrue(result == ";")
+        self.assertEqual(result, ";")
 
     def test_parse_check_numstring_with_custom_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse(";\n1;2").numstring
-        self.assertTrue(result == "1,2")
+        self.assertEqual(result, "1,2")
 
     def test_parse_check_numbers_with_custom_delim(self):
         parser = StrCalculatorParser()
         result = parser.parse(";\n1;2").numbers
-        self.assertTrue(result == [1, 2])
+        self.assertEqual(result, [1, 2])
