c6024747bed6dd91b0ef1ce7ece187e55f388d4e
Svetlana Kulikova <Svetlana.Kulikova@onlyoffice.com>
2020-11-10 18:11:09 +0300

    fix length

--- code/fraction/model/test_kulikova_svetlana.py
+++ code/fraction/model/test_kulikova_svetlana.py
@@ -14,18 +14,8 @@ class TestFraction(unittest.TestCase):
         res = frac1 + frac2
         self.assertTrue(res.is_equal(7, 6))
         
-    def test_sub(self):
-        frac1 = Fraction(1, 2)
-        frac2 = Fraction(2, 3)
-        res = frac1 - frac2
-        self.assertTrue(res.is_equal(-1, 6))
-        
     def test_mul(self):
         frac1 = Fraction(1, 2)
         frac2 = Fraction(2, 3)
         res = frac1 * frac2
         self.assertTrue(res.is_equal(1, 3))
-        
-    def test_mul(self):
-        frac = Fraction(2, 4)
-        self.assertTrue(frac.is_equal(1, 2))
