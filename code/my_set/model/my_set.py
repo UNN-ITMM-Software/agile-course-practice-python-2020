2f1c1706026faf896b35daea5915f3550d5929c8
Usova Marina Andreevna <oppabang@mail.ru>
2020-11-11 00:26:45 +0300

    Added function for get size and test

--- code/my_set/model/my_set.py
+++ code/my_set/model/my_set.py
@@ -2,3 +2,6 @@ class MySet:
     # The class sets using lists
     def __init__(self):
         self.my_set = list()
+
+    def get_size(self):
+        return 0
--- code/my_set/model/test_my_set.py
+++ code/my_set/model/test_my_set.py
@@ -7,3 +7,8 @@ class TestSetClass(unittest.TestCase):
     def test_can_init_with_default_constructor(self):
         test_set = MySet()
         self.assertTrue(isinstance(test_set, MySet))
+
+    def test_can_get_size_of_set(self):
+        test_set = MySet()
+        size = test_set.get_size()
+        self.assertEqual(size, 0)
