184d795d66613adc46e1a9e415636f4084498747
GerasimovDG <gerasimov-dmr@yandex.ru>
2020-11-10 10:32:03 +0300

    fixed issue from @okainov 's comment

--- code/gerasimov_dmitry_lab1/solving_quadratic_equation.py
+++ code/gerasimov_dmitry_lab1/solving_quadratic_equation.py
@@ -1,25 +1,50 @@
 import math
 
-NO_SOLUTION = "No solution"
+
+def is_correct_input_type(value):
+    if type(value) is float or type(value) is int:
+        return True
+    return False
 
 
-def solving_quadratic_equation(a, b, c):
-    if a == 0:
-        raise ValueError("The equation is not square. The first argument cannot be zero")
-    disc = b ** 2 - 4 * a * c
-    if b == 0:
-        if c == 0:
-            disc = 0
-        elif a > 0 and c > 0 or a < 0 and c < 0:
-            return NO_SOLUTION
+class QuadraticEquation:
+    def __init__(self, a=1, b=0, c=0):
+        if not is_correct_input_type(a) or not is_correct_input_type(b) or not is_correct_input_type(c):
+            raise TypeError("The arguments type must be 'float' or 'int'")
+        if a == 0:
+            raise ValueError("The equation is not square. The first argument cannot be zero")
+        self.answer = None
+        self.a = a
+        self.b = b
+        self.c = c
 
-    if disc < 0:
-        return NO_SOLUTION
-    elif disc == 0:
-        x = (-b / (2 * a))
-        return x
-    else:
-        x1 = (-b + math.sqrt(disc)) / (2 * a)
-        x2 = (-b - math.sqrt(disc)) / (2 * a)
-        res = sorted([x1, x2])
-    return res[0], res[1]
+    def solution(self):
+        self.answer = list()
+        disc = self.b ** 2 - 4 * self.a * self.c
+        if self.b == 0:
+            if self.c == 0:
+                disc = 0
+            elif self.a > 0 and self.c > 0 or self.a < 0 and self.c < 0:
+                return self.answer
+
+        if disc < 0:
+            return self.answer
+        elif disc == 0:
+            x = (-self.b / (2 * self.a))
+            self.answer.append(x)
+        else:
+            x1 = (-self.b + math.sqrt(disc)) / (2 * self.a)
+            x2 = (-self.b - math.sqrt(disc)) / (2 * self.a)
+            self.answer.extend([x1, x2])
+            self.answer.sort()
+        return self.answer
+
+    def solution_in_string_format(self):
+        if not self.answer:
+            self.solution()
+        if len(self.answer) == 0:
+            return 'No solution'
+        elif len(self.answer) == 1:
+            return 'Answer:\n x = {}'.format(self.answer[0])
+        else:
+            return 'Answers:\n x1 = {},\n x2 = {}'.format(self.answer[0], self.answer[1])
