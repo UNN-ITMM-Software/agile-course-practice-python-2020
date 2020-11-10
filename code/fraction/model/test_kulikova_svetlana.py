bee881420da9af01014eff9961e5cadddfcbc689
Svetlana Kulikova <Svetlana.Kulikova@onlyoffice.com>
2020-11-10 18:15:22 +0300

    fix max-length

--- code/fraction/model/test_kulikova_svetlana.py
+++ code/fraction/model/test_kulikova_svetlana.py
@@ -1,19 +1,15 @@
 import unittest
 
-from fraction.model.fraction import Fraction, InvalidFractionError
+from fraction.model.fraction import Fraction
 
 
 class TestFraction(unittest.TestCase):
-    def test_init_error(self):
-        with self.assertRaises(InvalidFractionError):
-            Fraction(1, 0)
-
     def test_sum(self):
         frac1 = Fraction(1, 2)
         frac2 = Fraction(2, 3)
         res = frac1 + frac2
         self.assertTrue(res.is_equal(7, 6))
-        
+
     def test_mul(self):
         frac1 = Fraction(1, 2)
         frac2 = Fraction(2, 3)
