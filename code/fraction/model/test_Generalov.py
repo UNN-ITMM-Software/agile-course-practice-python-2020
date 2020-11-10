1a1b54d361d2336bdb34c0893ca367fdab4a9e95
Aleksandr <aleksandr-generalov19@yandex.ru>
2020-11-10 21:22:01 +0300

    Added more unit tests

---
+++ code/fraction/model/test_Generalov.py
@@ -0,0 +1,21 @@
+import unittest
+
+from fraction.model.fraction import Fraction
+
+
+class TestFraction(unittest.TestCase):
+    def test_create_fraction(self):
+        frac = Fraction()
+        self.assertTrue(isinstance(frac, Fraction))
+
+    def test_multiply_fraction(self):
+        frac_1 = Fraction(1, 3)
+        frac_2 = Fraction(3, 4)
+        result = frac_1 * frac_2
+        self.assertTrue(result.is_equal(3, 8))
+
+    def test_sum_fraction_1_3_3_4(self):
+        frac_1 = Fraction(1, 3)
+        frac_2 = Fraction(3, 4)
+        result = frac_1 + frac_2
+        self.assertTrue(result.is_equal(5, 4))
