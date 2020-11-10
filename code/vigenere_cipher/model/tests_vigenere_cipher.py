55b464aa131b010092bd19607fa61f249aba00e4
Svetlana Kulikova <Svetlana.Kulikova@onlyoffice.com>
2020-11-10 18:39:36 +0300

    empty files

---
+++ code/vigenere_cipher/model/tests_vigenere_cipher.py
@@ -0,0 +1,9 @@
+import unittest
+
+from vigenere_cipher.model.vigenere_cipher import VigenereCipher
+
+
+class TestVigenereCipher(unittest.TestCase):
+    def test_init(self):
+        cipher = VigenereCipher();
+        self.assertTrue(isinstance(cipher, VigenereCipher)
---
+++ code/vigenere_cipher/model/vigenere_cipher.py
@@ -0,0 +1,5 @@
+
+class VigenereCipher:
+    def __init__(self, key):
+        if(
+        self.key = [k.lower() for k in key];
