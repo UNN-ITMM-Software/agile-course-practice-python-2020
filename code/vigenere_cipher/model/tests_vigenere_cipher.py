7075ce14b609f69ed449e0512d2d2fba05b2e483
Svetlana Kulikova <Svetlana.Kulikova@onlyoffice.com>
2020-11-10 23:25:36 +0300

    fix

--- code/vigenere_cipher/model/tests_vigenere_cipher.py
+++ code/vigenere_cipher/model/tests_vigenere_cipher.py
@@ -5,5 +5,5 @@ from vigenere_cipher.model.vigenere_cipher import VigenereCipher
 
 class TestVigenereCipher(unittest.TestCase):
     def test_init(self):
-        cipher = VigenereCipher();
-        self.assertTrue(isinstance(cipher, VigenereCipher)
+        cipher = VigenereCipher()
+        self.assertTrue(isinstance(cipher, VigenereCipher))
--- code/vigenere_cipher/model/vigenere_cipher.py
+++ code/vigenere_cipher/model/vigenere_cipher.py
@@ -1,5 +1,11 @@
+import re
+
 
 class VigenereCipher:
-    def __init__(self, key):
-        if(
-        self.key = [k.lower() for k in key];
+    def __init__(self, key=""):
+        self.key = ""
+        if not isinstance(key, str):
+            raise TypeError("The key type must be 'string'")
+        for k in key:
+            if re.search(r'[^a-zA-Z]', k):
+                self.key += k.lower()
