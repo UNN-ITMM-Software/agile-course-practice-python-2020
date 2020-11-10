ded4a70d554eb020d01fa2c3833972987b856043
Svetlana Kulikova <Svetlana.Kulikova@onlyoffice.com>
2020-11-10 17:17:48 +0300

    added tests

---
+++ code/fraction/model/test_kulikova_svetlana.py
@@ -0,0 +1,31 @@
+import unittest
+
+from fraction.model.fraction import Fraction, InvalidFractionError
+
+
+class TestFraction(unittest.TestCase):
+    def test_init_error(self):
+        with self.assertRaises(InvalidFractionError):
+            Fraction(1, 0)
+
+    def test_sum(self):
+        frac1 = Fraction(1, 2)
+        frac2 = Fraction(2, 3)
+        res = frac1 + frac2
+        self.assertTrue(res.is_equal(7, 6))
+        
+    def test_sub(self):
+        frac1 = Fraction(1, 2)
+        frac2 = Fraction(2, 3)
+        res = frac1 + frac2
+        self.assertTrue(res.is_equal(-1, 6))
+        
+    def test_mul(self):
+        frac1 = Fraction(1, 2)
+        frac2 = Fraction(2, 3)
+        res = frac1 + frac2
+        self.assertTrue(res.is_equal(1, 3))
+        
+    def test_mul(self):
+        frac = Fraction(2, 4)
+        self.assertTrue(frac.is_equal(1, 2))
