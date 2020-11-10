0c85c50573afa9a3a12d679e84c5eae5ed44bba2
Tarakanov Kirill <tarakan.php@yandex.ru>
2020-11-10 12:35:30 +0300

    Add more tests for size and is_empty methods

--- code/tarakanov_kirill_lab1/model/test_modify_stack.py
+++ code/tarakanov_kirill_lab1/model/test_modify_stack.py
@@ -22,11 +22,19 @@ class TestModifyStack(unittest.TestCase):
 
     def test_is_empty(self):
         stack = ModifyStack()
-        self.assertTrue(stack.is_empty(), True)
+        self.assertTrue(stack.is_empty())
+
+    def test_in_not_empty(self):
+        stack = ModifyStack([1, 2, 3, 4])
+        self.assertFalse(stack.is_empty())
 
     def test_size_return_zero_for_empty_stack(self):
         stack = ModifyStack()
         self.assertEqual(stack.size(), 0)
+
+    def test_size_not_zero(self):
+        stack = ModifyStack([1, 2, 3, 4])
+        self.assertEqual(stack.size(), 4)
 
     def test_push_in_empty_stack_and_look_top_then(self):
         stack = ModifyStack()
