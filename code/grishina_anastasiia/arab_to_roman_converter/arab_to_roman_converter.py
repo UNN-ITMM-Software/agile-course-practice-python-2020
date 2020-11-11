91c4ebefff3f270f931292c669eaa38948016f45
Nastya Sopina <Nastya Sopina>
2020-11-11 21:23:56 +0300

    Refactoring

--- code/grishina_anastasiia/arab_to_roman_converter/arab_to_roman_converter.py
+++ code/grishina_anastasiia/arab_to_roman_converter/arab_to_roman_converter.py
@@ -2,14 +2,16 @@ def get_ones_of_number():
     return ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VII', 'IX']
 
 
+def get_tens_of_number():
+    return ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+
+
 class ArabToRomanConverter:
 
     def __init__(self, number):
         self.number = number
 
     def convert_arab_to_roman_number(self):
-        if self.number == 10:
-            return 'X'
-        elif self.number == 70:
-            return 'LXX'
+        if self.number == 10 or self.number == 70:
+            return get_tens_of_number()[int(self.number / 10)]
         return get_ones_of_number()[self.number]
