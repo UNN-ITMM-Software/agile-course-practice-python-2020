4c80eb23aa5359b9581e9fc780841bc90b69be3c
Samarkina Valerija <samarkina.valerija@gmail.com>
2020-11-11 19:55:19 +0300

    Add Test and method Amount

--- code/statistics/register.py
+++ code/statistics/register.py
@@ -1,1 +1,14 @@
 class Register(object):
+    @staticmethod
+    def is_correct(marks):
+        for i in marks:
+            if i not in [1, 2, 3, 4, 5]:
+                raise ValueError
+        return True
+
+    def __init__(self, array_of_marks: list):
+        if Register.is_correct(array_of_marks):
+            self.marks = array_of_marks
+
+    def amount(self):
+        return len(self.marks)
--- code/statistics/test_register.py
+++ code/statistics/test_register.py
@@ -3,5 +3,19 @@ import unittest
 from register import Register
 
 
+class TestAmountOfMarks(unittest.TestCase):
+    def test_correct_amount(self):
+        result = Register([2, 3]).amount()
+        self.assertEqual(result, 2)
+
+    def test_amount_not_negative(self):
+        result = Register([2, 3]).amount()
+        self.assertGreaterEqual(result, 0)
+
+    def test_amount_not_string(self):
+        result = Register([2, 3]).amount()
+        self.assertNotIsInstance(result, str)
+
+
 if __name__ == '__main__':
     unittest.main()
