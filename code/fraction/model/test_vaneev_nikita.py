09b285321ac12816584f90374a025174619821ed
Nikita Vaneev <vaneev.nikita2014@yandex.ru>
2020-11-11 14:18:07 +0300

    Fixing issues

--- code/fraction/model/test_vaneev_nikita.py
+++ code/fraction/model/test_vaneev_nikita.py
@@ -5,10 +5,10 @@ from fraction.model.fraction import Fraction
 
 class TestFractionClass(unittest.TestCase):
 
-    def test_can_create_7_2_fraction(self):
-        frac = Fraction(7, 2)
-        self.assertTrue(frac.is_equal(7, 2))
+    def test_can_create_1_4_fraction(self):
+        frac = Fraction(1, 4)
+        self.assertTrue(frac.is_equal(1, 4))
 
-    def test_can_create_4_6_fraction(self):
-        frac = Fraction(4, 6)
-        self.assertTrue(frac.is_equal(4, 6))
+    def test_can_create_1_5_fraction(self):
+        frac = Fraction(1, 5)
+        self.assertTrue(frac.is_equal(1, 5))
