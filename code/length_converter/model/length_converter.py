f6f56da47f5c723b04c419d01380a812c8ec9328
Anton Grishin <anton.grishin@intel.com>
2020-11-11 19:34:42 +0300

    add convert function and test

--- code/length_converter/model/length_converter.py
+++ code/length_converter/model/length_converter.py
@@ -3,3 +3,6 @@ class LengthConverter:
     def __init__(self, value, length_type):
         self.value = value
         self.length_type = length_type
+
+    def convert(self, to_length_type):
+        return 0
--- code/length_converter/model/tests_for_length_converter.py
+++ code/length_converter/model/tests_for_length_converter.py
@@ -7,3 +7,8 @@ class TemperatureConverterTest(unittest.TestCase):
     def test_can_create_converter(self):
         converter = LengthConverter(0, 0)
         self.assertTrue(isinstance(converter, LengthConverter))
+
+    def test_can_convert_zero_meter_to_meter(self):
+        converter = LengthConverter(0, 0)
+        meter = converter.convert(0)
+        self.assertEqual(meter, 0)
