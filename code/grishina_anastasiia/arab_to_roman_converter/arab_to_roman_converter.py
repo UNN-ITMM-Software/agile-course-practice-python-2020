ba70d1bc575028b1d6a6decfeb4564fc8554d1b8
Nastya Sopina <Nastya Sopina>
2020-11-11 21:48:10 +0300

    Refactoring

--- code/grishina_anastasiia/arab_to_roman_converter/arab_to_roman_converter.py
+++ code/grishina_anastasiia/arab_to_roman_converter/arab_to_roman_converter.py
@@ -6,6 +6,10 @@ def get_tens_of_number():
     return ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
 
 
+def get_hundreds_of_number():
+    return ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+
+
 class ArabToRomanConverter:
 
     def __init__(self, number):
@@ -14,12 +18,6 @@ class ArabToRomanConverter:
     def convert_arab_to_roman_number(self):
         one = get_ones_of_number()[self.number % 10]
         ten = get_tens_of_number()[int(self.number / 10) % 10]
+        hundred = get_hundreds_of_number()[int(self.number / 100) % 10]
 
-        if self.number == 100:
-            return 'C'
-        elif self.number == 150:
-            return 'C' + ten
-        elif self.number == 288:
-            return 'CC' + ten + one
-
-        return ten + one
+        return hundred + ten + one
