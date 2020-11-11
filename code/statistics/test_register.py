0810b30a38e06144da677655ea8dfe9a00c30d7e
Samarkina Valerija <samarkina.valerija@gmail.com>
2020-11-11 19:57:01 +0300

    Add tests

--- code/statistics/test_register.py
+++ code/statistics/test_register.py
@@ -17,5 +17,19 @@ class TestAmountOfMarks(unittest.TestCase):
         self.assertNotIsInstance(result, str)
 
 
+class TestCorrectInputValue(unittest.TestCase):
+    def test_marks_return_error_message_if_arguments_str(self):
+        self.assertRaises(ValueError, Register.is_correct, ['two', 'three', 'four', 'five'])
+
+    def test_marks_return_error_message_if_arguments_negative(self):
+        self.assertRaises(ValueError, Register.is_correct, [-2, 3, -4, 5])
+
+    def test_marks_return_error_message_if_arguments_less_1(self):
+        self.assertRaises(ValueError, Register.is_correct, [3, 1, 0, 5])
+
+    def test_marks_return_error_message_if_arguments_greater_5(self):
+        self.assertRaises(ValueError, Register.is_correct, [4, 3, 7, 5])
+
+
 if __name__ == '__main__':
     unittest.main()
