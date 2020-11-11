8f7eda8476864e56c2b5033cb3edbd0e620ec3c2
Svetlana Kulikova <Svetlana.Kulikova@onlyoffice.com>
2020-11-11 23:59:22 +0300

    encipher

--- code/vigenere_cipher/model/test_vigenere_cipher.py
+++ code/vigenere_cipher/model/test_vigenere_cipher.py
@@ -9,18 +9,18 @@ class TestVigenereCipher(unittest.TestCase):
         self.assertTrue(isinstance(cipher, VigenereCipher))
 
     def test_key_good(self):
-        key = "amo"
+        key = "AMO"
         cipher = VigenereCipher(key)
         self.assertEqual(key, cipher.key)
 
-    def test_key_to_lower(self):
-        key = "AmO"
+    def test_key_to_upper(self):
+        key = "aMo"
         cipher = VigenereCipher(key)
-        self.assertEqual(key.lower(), cipher.key)
+        self.assertEqual(key.upper(), cipher.key)
 
     def test_key_remove_bad_characters(self):
         key = "A m1oЪ*"
-        res = "amo"
+        res = "AMO"
         cipher = VigenereCipher(key)
         self.assertEqual(res, cipher.key)
 
@@ -35,3 +35,54 @@ class TestVigenereCipher(unittest.TestCase):
     def test_key_all_bad_characters(self):
         key = " 1Ъ*"
         self.assertRaises(ValueError, VigenereCipher, key)
+
+    def test_encipher_good(self):
+        key = "amo"
+        string = "abba"
+        res = "ANPA"
+        cipher = VigenereCipher(key)
+        self.assertEqual(res, cipher.encipher(string))
+
+    def test_encipher_adding_non_letters(self):
+        key = "amo"
+        string = "a b b a"
+        res = "A N P A"
+        cipher = VigenereCipher(key)
+        self.assertEqual(res, cipher.encipher(string))
+
+    def test_i2a_good(self):
+        i = 53
+        cipher = VigenereCipher()
+        res = 'B'
+        self.assertEqual(res, cipher.i2a(i))
+
+    def test_i2a_type_error(self):
+        i = "amo"
+        cipher = VigenereCipher()
+        self.assertRaises(TypeError, cipher.i2a, i)
+
+    def test_i2a_value_error(self):
+        i = -5
+        cipher = VigenereCipher()
+        self.assertRaises(ValueError, cipher.i2a, i)
+
+    def test_a2i_good(self):
+        ch = 'B'
+        cipher = VigenereCipher()
+        res = 1
+        self.assertEqual(res, cipher.a2i(ch))
+
+    def test_a2i_type_error(self):
+        ch = 1
+        cipher = VigenereCipher()
+        self.assertRaises(TypeError, cipher.a2i, ch)
+
+    def test_a2i_one_letter(self):
+        ch = "amo"
+        cipher = VigenereCipher()
+        self.assertRaises(ValueError, cipher.a2i, ch)
+
+    def test_a2i_english(self):
+        ch = "ф"
+        cipher = VigenereCipher()
+        self.assertRaises(ValueError, cipher.a2i, ch)
