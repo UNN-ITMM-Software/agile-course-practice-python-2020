2d453d06274e2b417cbdfa9fbef3a8a901a1bcf1
Andrey Kalistratov <Farvata@mail.ru>
2020-11-12 15:20:59 +0300

    refactoring the counting_neighbors class

--- code/game_of_life/game_of_life.py
+++ code/game_of_life/game_of_life.py
@@ -39,27 +39,35 @@ class GameOfLife(object):
 
     def counting_neighbors(self, current_field, y, x):
         count = 0
-        step_back_y = 1
-        step_back_x = 1
-        step_forward_y = 1
-        step_forward_x = 1
-        height_field = len(current_field)
-        width_field = len(current_field[0])
-        if y == 0:
-            step_back_y = 0
-        if x == 0:
-            step_back_x = 0
-        if y == height_field - 1:
-            step_forward_y = 0
-        if x == width_field - 1:
-            step_forward_x = 0
-        for i in range(y - step_back_y, y + step_forward_y + 1):
-            for j in range(x - step_back_x, x + step_forward_x + 1):
+        step = self.calc_border_of_neighbors(current_field, y, x)
+        for i in range(y - step["step_back_y"], y + step["step_forward_y"] + 1):
+            for j in range(x - step["step_back_x"], x + step["step_forward_x"] + 1):
                 if current_field[i][j]:
                     count += 1
         if current_field[y][x]:
             count -= 1
         return count
+
+    def calc_border_of_neighbors(self, current_field, y, x):
+        step = {}
+        step_back_y = 1
+        step_back_x = 1
+        step_forward_y = 1
+        step_forward_x = 1
+        field_boundaries = self.calc_field_boundaries(current_field)
+        if y == 0:
+            step_back_y = 0
+        if x == 0:
+            step_back_x = 0
+        if y == field_boundaries["height_field"] - 1:
+            step_forward_y = 0
+        if x == field_boundaries["width_field"] - 1:
+            step_forward_x = 0
+        step["step_back_y"] = step_back_y
+        step["step_back_x"] = step_back_x
+        step["step_forward_y"] = step_forward_y
+        step["step_forward_x"] = step_forward_x
+        return step
 
     def convert_to_string(self, current_field):
         height_field = len(current_field)
