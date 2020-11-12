ffc0330d5252c597aaab2c1e78ed47dfcff6f1b7
LikHait <hait.08@mail.ru>
2020-11-12 20:05:40 +0300

    Implement multiple insert

--- code/hashmap/model/hashmap.py
+++ code/hashmap/model/hashmap.py
@@ -1,15 +1,18 @@
 
 class Hashmap(object):
 
-    def __init__(self):
-        self.__obj = None
+    def __init__(self, size=113):
+        self.__size = size
+        self.__obj = [None for _ in range(size)]
         pass
 
     def insert(self, key, value):
-        self.__obj = value
+        h = hash(key) % self.__size
+        self.__obj[h] = (key, value)
 
     def get(self, key):
-        return self.__obj
+        h = hash(key) % self.__size
+        return self.__obj[h][1]
 
     def delete(self, key):
         pass
--- code/hashmap/model/tests_for_hashmap.py
+++ code/hashmap/model/tests_for_hashmap.py
@@ -9,8 +9,21 @@ class TestHashmap(unittest.TestCase):
         Hashmap()
 
     def test_can_insert_and_get_item(self):
-        map = Hashmap()
+        hmap = Hashmap()
         obj = "zero"
-        map.insert(0, obj)
-        res = map.get(0)
+        hmap.insert(0, obj)
+        res = hmap.get(0)
         self.assertEqual(obj, res)
+
+    def test_can_insert_few_items_and_get_items(self):
+        hmap = Hashmap()
+        key1 = 0
+        key2 = 1
+        obj1 = "zero"
+        obj2 = "one"
+        hmap.insert(key1, obj1)
+        hmap.insert(key2, obj2)
+        res1 = hmap.get(key1)
+        res2 = hmap.get(key2)
+        self.assertEqual(obj1, res1)
+        self.assertEqual(obj2, res2)
