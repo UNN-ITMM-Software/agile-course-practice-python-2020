f725e2c8f89e749a994ed1eb2d665066d00f5091
sshutina <sshutina>
2020-11-12 21:01:13 +0300

    Lab 1: Batcher sort

---
+++ code/ShutinaSvetlana/batcher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import math
+
+class Sorting:
+    # class realizes the sorting for array of numbers
+    arr =[]
+    def __init__(self, arr):
+        self.arr = arr
+        
+    def batcher_sort(self):
+        t = int(math.log(len(self.arr),2))
+        p = 2 ** (t - 1)
+        while p > 0:
+            q = 2 ** (t - 1)
+            r = 0
+            d = p
+            while q >= p:
+                i = 0
+                while i < len(self.arr) - d:
+                    if self.arr[i] > self.arr[i + d]:
+                        self.arr[i], self.arr[i + d] = self.arr[i + d], self.arr[i]
+                    i += 1
+                d = q - p
+                q = int(q / 2)
+                r = p
+            p = p // 2
+    def result_str(self):
+        string = ''
+        for j in range(len(self.arr)):
+            space = ''
+            if j < len(self.arr) - 1:
+                space = ' '
+            string += str(self.arr[j]) + space
+        return string
+
---
+++ code/ShutinaSvetlana/test_batcher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import unittest
+
+from ShutinaSvetlana.batcher import Sorting
+
+class TestBatcherSortClass(unittest.TestCase):
+        def test_sort_5(self):
+            arr = [5, 6, 1, 2, 0]
+            result = Sorting(arr)
+            result.batcher_sort()
+            self.assertEqual('0 1 2 5 6', str(result.result_str()))     
+        def test_sort_15(self):
+            arr = [5, 6, 1, 2, 0, 100, -5, 6, -10, 20, 5, 6, 1, 36, 25]
+            result = Sorting(arr)
+            result.batcher_sort()
+            self.assertEqual('-10 -5 0 1 1 2 5 5 6 6 6 20 25 36 100', str(result.result_str())) 
+        def test_sort_10(self):
+            arr = [5, 6, 1, 2, 0, 0.5, -2.5, 10, 20, 9, 10]
+            result = Sorting(arr)
+            result.batcher_sort()
+            self.assertEqual('-2.5 0 0.5 1 2 5 6 9 10 10 20', str(result.result_str()))       
+
