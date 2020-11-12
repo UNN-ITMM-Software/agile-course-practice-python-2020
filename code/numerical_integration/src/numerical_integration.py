448489ee26c208fb7358a813d6939469aea704b7
Nikita Vaneev <vaneev.nikita2014@yandex.ru>
2020-11-12 16:43:36 +0300

    Refactor code.

--- code/numerical_integration/src/numerical_integration.py
+++ code/numerical_integration/src/numerical_integration.py
@@ -1,19 +1,20 @@
 class NumericalIntegrator(object):
     N = 1000
-    def trapeziumMethod(self, a, b, func):
+
+    def trapezium_method(self, a, b, func):
         width = (b - a) / self.N
-        trapezoidalIntegral = 0
-        for i in range(self.N): 
-            x1 = a + i * width;
-            x2 = a + (i + 1) * width;
-            trapezoidalIntegral += 0.5 * (x2 - x1) * (func(x1) + func(x2))
-        return trapezoidalIntegral
-    
-    def simpsonMethod(self, a, b, func):
+        trapezoidal_integral = 0
+        for i in range(self.N):
+            x1 = a + i * width
+            x2 = a + (i + 1) * width
+            trapezoidal_integral += 0.5 * (x2 - x1) * (func(x1) + func(x2))
+        return trapezoidal_integral
+
+    def simpson_method(self, a, b, func):
         width = (b - a) / self.N
-        simpsonIntegral = 0
-        for i in range(self.N): 
-            x1 = a + i * width;
-            x2 = a + (i + 1) * width;
-            simpsonIntegral += (x2 - x1) / 6.0 * (func(x1) + 4.0 * func(0.5 * (x1 + x2)) + func(x2))
-        return simpsonIntegral
+        simpson_integral = 0
+        for i in range(self.N):
+            x1 = a + i * width
+            x2 = a + (i + 1) * width
+            simpson_integral += (x2 - x1) / 6.0 * (func(x1) + 4.0 * func(0.5 * (x1 + x2)) + func(x2))
+        return simpson_integral
--- code/numerical_integration/src/numerical_integration_test.py
+++ code/numerical_integration/src/numerical_integration_test.py
@@ -2,91 +2,93 @@ import unittest
 
 from numerical_integration import NumericalIntegrator
 
+
 class TestNumericalIntegratorClass(unittest.TestCase):
+
     def test_numerical_integratior_can_create(self):
-        numericalIntegratior = NumericalIntegrator()
-        self.assertTrue(isinstance(numericalIntegratior, NumericalIntegrator))
+        numerical_integratior = NumericalIntegrator()
+        self.assertTrue(isinstance(numerical_integratior, NumericalIntegrator))
 
-    def test_numerical_integratior_trapeziumMethod_func_zero(self):
-        def func(x): 
+    def test_numerical_integratior_trapezium_method_func_zero(self):
+        def func(x):
             return 0
-        result = NumericalIntegrator().trapeziumMethod(1, 2, func)
+        result = NumericalIntegrator().trapezium_method(1, 2, func)
         self.assertEqual(result, 0)
 
-    def test_numerical_integratior_trapeziumMethod_func_linear(self):
-        def func(x): 
+    def test_numerical_integratior_trapezium_method_func_linear(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().trapeziumMethod(1, 2, func)
-        self.assertEqual(round(result,5), round(1.5,5))
+        result = NumericalIntegrator().trapezium_method(1, 2, func)
+        self.assertEqual(round(result, 5), round(1.5, 5))
 
-    def test_numerical_integratior_trapeziumMethod_func_difficult(self):
-        def func(x): 
+    def test_numerical_integratior_trapezium_method_func_difficult(self):
+        def func(x):
             return x * (x + 1) + 1
-        result = NumericalIntegrator().trapeziumMethod(1, 2, func)
+        result = NumericalIntegrator().trapezium_method(1, 2, func)
         self.assertEqual(round(result, 5), round(4.83333, 5))
 
-    def test_numerical_integratior_trapeziumMethod_args_zero(self):
-        def func(x): 
+    def test_numerical_integratior_trapezium_method_args_zero(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().trapeziumMethod(0, 0, func)
+        result = NumericalIntegrator().trapezium_method(0, 0, func)
         self.assertEqual(result, 0)
 
-    def test_numerical_integratior_trapeziumMethod_args_positive_sub_negative(self):
-        def func(x): 
+    def test_numerical_integratior_trapezium_method_args_positive_sub_negative(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().trapeziumMethod(2, 1, func)
-        self.assertEqual(round(result,5), round(-1.5,5))
+        result = NumericalIntegrator().trapezium_method(2, 1, func)
+        self.assertEqual(round(result, 5), round(-1.5, 5))
 
-    def test_numerical_integratior_trapeziumMethod_args_negative_sub_positive(self):
-        def func(x): 
+    def test_numerical_integratior_trapezium_method_args_negative_sub_positive(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().trapeziumMethod(-2, -1, func)
-        self.assertEqual(round(result,5), round(-1.5,5))
+        result = NumericalIntegrator().trapezium_method(-2, -1, func)
+        self.assertEqual(round(result, 5), round(-1.5, 5))
 
-    def test_numerical_integratior_trapeziumMethod_args_negative_sub_negative(self):
-        def func(x): 
+    def test_numerical_integratior_trapezium_method_args_negative_sub_negative(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().trapeziumMethod(-1, -2, func)
-        self.assertEqual(round(result,5), round(1.5,5))
+        result = NumericalIntegrator().trapezium_method(-1, -2, func)
+        self.assertEqual(round(result, 5), round(1.5, 5))
 
-    def test_numerical_integratior_simpsonMethod_func_zero(self):
-        def func(x): 
+    def test_numerical_integratior_simpson_method_func_zero(self):
+        def func(x):
             return 0
-        result = NumericalIntegrator().simpsonMethod(1, 2, func)
+        result = NumericalIntegrator().simpson_method(1, 2, func)
         self.assertEqual(result, 0)
 
-    def test_numerical_integratior_simpsonMethod_func_linear(self):
-        def func(x): 
+    def test_numerical_integratior_simpson_method_func_linear(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().simpsonMethod(1, 2, func)
-        self.assertEqual(round(result,5), round(1.5,5))
+        result = NumericalIntegrator().simpson_method(1, 2, func)
+        self.assertEqual(round(result, 5), round(1.5, 5))
 
-    def test_numerical_integratior_simpsonMethod_func_difficult(self):
-        def func(x): 
+    def test_numerical_integratior_simpson_method_func_difficult(self):
+        def func(x):
             return x * (x + 1) + 1
-        result = NumericalIntegrator().simpsonMethod(1, 2, func)
+        result = NumericalIntegrator().simpson_method(1, 2, func)
         self.assertEqual(round(result, 5), round(4.83333, 5))
 
-    def test_numerical_integratior_simpsonMethod_args_zero(self):
-        def func(x): 
+    def test_numerical_integratior_simpson_method_args_zero(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().simpsonMethod(0, 0, func)
+        result = NumericalIntegrator().simpson_method(0, 0, func)
         self.assertEqual(result, 0)
 
-    def test_numerical_integratior_simpsonMethod_args_positive_sub_negative(self):
-        def func(x): 
+    def test_numerical_integratior_simpson_method_args_positive_sub_negative(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().simpsonMethod(2, 1, func)
-        self.assertEqual(round(result,5), round(-1.5,5))
+        result = NumericalIntegrator().simpson_method(2, 1, func)
+        self.assertEqual(round(result, 5), round(-1.5, 5))
 
-    def test_numerical_integratior_simpsonMethod_args_negative_sub_positive(self):
-        def func(x): 
+    def test_numerical_integratior_simpson_method_args_negative_sub_positive(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().simpsonMethod(-2, -1, func)
-        self.assertEqual(round(result,5), round(-1.5,5))
+        result = NumericalIntegrator().simpson_method(-2, -1, func)
+        self.assertEqual(round(result, 5), round(-1.5, 5))
 
-    def test_numerical_integratior_simpsonMethod_args_negative_sub_negative(self):
-        def func(x): 
+    def test_numerical_integratior_simpson_method_args_negative_sub_negative(self):
+        def func(x):
             return x
-        result = NumericalIntegrator().simpsonMethod(-1, -2, func)
-        self.assertEqual(round(result,5), round(1.5,5))
+        result = NumericalIntegrator().simpson_method(-1, -2, func)
+        self.assertEqual(round(result, 5), round(1.5, 5))
