eac61fd766c63f7a27b41b27aff77e0596c2ee27
AleksandrPanov <sasha1_1997@mail.ru>
2020-11-12 15:28:35 +0300

    added gcd to fix reduce test

--- code/rational_number/model/rational_number.py
+++ code/rational_number/model/rational_number.py
@@ -9,9 +9,15 @@ class RationalNumber:
         self._reduce()
 
     def _reduce(self):
+        gcd = self._gcd(max(self._num, self._den), min(self._num, self._den))
+        self._num //= gcd
+        self._den //= gcd
         if self._den < 0:
             self._den *= -1
             self._num *= -1
+
+    def _gcd(self, a, b):
+        return a if b == 0 else self._gcd(b, a % b)
 
     def __eq__(self, other):
         return self._den == other._den and self._num == other._num
