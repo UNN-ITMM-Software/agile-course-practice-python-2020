cf2c2c781847000356345d0f0e5d2d3ca733e0df
Nikita Vaneev <vaneev.nikita2014@yandex.ru>
2020-11-12 15:16:10 +0300

    Add implimentation trapeziumMethod in NumericalIntegrator class.

--- code/numerical_integration/src/numerical_integration.py
+++ code/numerical_integration/src/numerical_integration.py
@@ -1,7 +1,13 @@
 class NumericalIntegrator(object):
-    
+    N = 1000
     def trapeziumMethod(self, a, b, func):
-        pass
+        width = (b - a) / self.N
+        trapezoidalIntegral = 0
+        for i in range(self.N): 
+            x1 = a + i * width;
+            x2 = a + (i + 1) * width;
+            trapezoidalIntegral += 0.5 * (x2 - x1) * (func(x1) + func(x2))
+        return trapezoidalIntegral
     
     def simpsonMethod(self, a, b, func):
         pass
