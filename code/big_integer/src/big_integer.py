fb0ac2a23c9e89a1b628b823cbccf79de1f5c29f
Andrey Gluzdov <adgluzdoff@gmail.com>
2020-11-12 17:18:33 +0300

    Add BigInteger class without implimentation.

---
+++ code/big_integer/src/big_integer.py
@@ -0,0 +1,18 @@
+class BigInteger(object):
+    BASE = 10
+    SIZE = 5
+
+    def __init__(self, digits = []):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __add__(self, other): 
+        pass
+
+    def __sub__(self, other): 
+        pass
+
+    def __mul__(self, other):
+        pass
