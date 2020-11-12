b1eee312b502d89340488a67cf16d18f9b32d1cc
Samarkina Valerija <samarkina.valerija@gmail.com>
2020-11-12 15:45:35 +0300

    Refactoring

---
+++ code/statistics/studentmarks.py
@@ -0,0 +1,62 @@
+class StudentMarks(object):
+    @staticmethod
+    def is_correct(list_of_marks):
+        for mark in list_of_marks:
+            if not isinstance(mark, int):
+                raise TypeError
+            if mark not in [1, 2, 3, 4, 5]:
+                raise ValueError
+        return True
+
+    def __init__(self, array_of_marks: list):
+        if StudentMarks.is_correct(array_of_marks):
+            self.marks = array_of_marks
+
+    def amount_of_marks(self):
+        return len(self.marks)
+
+    def average_of_marks(self):
+        summary = 0
+        count = self.amount_of_marks()
+        for i in self.marks:
+            summary += i
+        return summary / count
+
+
+class Register:
+    def __init__(self, students: list):
+        self.students = students
+
+    def add_new_student(self, student):
+        if StudentMarks.is_correct(student):
+            self.students.append(student)
+
+    def surnames_of_losers(self):
+        surnames = []
+        for student in self.students:
+            if student.average_of_marks() < 3:
+                surnames.append(student)
+        return surnames
+
+    def count_of_losers(self):
+        return len(self.surnames_of_losers())
+
+    def surnames_of_excellent_students(self):
+        surnames = []
+        for student in self.students:
+            if student.average_of_marks() >= 4.5:
+                surnames.append(student)
+        return surnames
+
+    def count_of_excellent(self):
+        return len(self.surnames_of_excellent_students())
+
+    def students_who_successfully_pass(self):
+        surnames = []
+        for student in self.students:
+            if student.average_of_marks() >= 4.5:
+                surnames.append(student)
+        return surnames
+
+    def count_of_students_who_successfully_pass(self):
+        return len(self.students_who_successfully_pass())
--- code/statistics/test_register.py
+++ code/statistics/test_register.py
@@ -1,47 +1,47 @@
 import unittest
 
-from student import Student, Register
+from studentmarks import StudentMarks, Register
 
 
 class TestAmountOfMarks(unittest.TestCase):
     def test_correct_amount(self):
-        result = Student([2, 3]).amount()
+        result = StudentMarks([2, 3]).amount_of_marks()
         self.assertEqual(result, 2)
 
     def test_amount_not_negative(self):
-        result = Student([2, 3]).amount()
+        result = StudentMarks([2, 3]).amount_of_marks()
         self.assertGreaterEqual(result, 0)
 
     def test_amount_not_string(self):
-        result = Student([2, 3]).amount()
+        result = StudentMarks([2, 3]).amount_of_marks()
         self.assertNotIsInstance(result, str)
 
 
 class TestCorrectInputValue(unittest.TestCase):
     def test_marks_return_error_message_if_arguments_str(self):
-        self.assertRaises(TypeError, Student.is_correct, ['two', 'three', 'four', 'five'])
+        self.assertRaises(TypeError, StudentMarks.is_correct, ['two', 'three', 'four', 'five'])
 
     def test_marks_return_error_message_if_arguments_negative(self):
-        self.assertRaises(ValueError, Student.is_correct, [-2, 3, -4, 5])
+        self.assertRaises(ValueError, StudentMarks.is_correct, [-2, 3, -4, 5])
 
     def test_marks_return_error_message_if_arguments_less_1(self):
-        self.assertRaises(ValueError, Student.is_correct, [3, 1, 0, 5])
+        self.assertRaises(ValueError, StudentMarks.is_correct, [3, 1, 0, 5])
 
     def test_marks_return_error_message_if_arguments_greater_5(self):
-        self.assertRaises(ValueError, Student.is_correct, [4, 3, 7, 5])
+        self.assertRaises(ValueError, StudentMarks.is_correct, [4, 3, 7, 5])
 
 
 class TestAverageMark(unittest.TestCase):
     def test_correct_average_mark(self):
-        result = Student([2, 3]).average()
+        result = StudentMarks([2, 3]).average_of_marks()
         self.assertEqual(2.5, result)
 
     def test_average_mark_not_negative(self):
-        result = Student([2, 3]).average()
+        result = StudentMarks([2, 3]).average_of_marks()
         self.assertGreaterEqual(result, 0)
 
     def test_average_mark_not_string(self):
-        result = Student([2, 3]).average()
+        result = StudentMarks([2, 3]).average_of_marks()
         self.assertNotIsInstance(result, str)
 
 
@@ -49,100 +49,127 @@ class TestAddStudentInRegister(unittest.TestCase):
     def test_add_student(self):
         journal = Register([])
         petrov = ([2, 3, 4, 5])
-        journal.add(petrov)
+        journal.add_new_student(petrov)
         self.assertIn(petrov, journal.students)
 
     def test_add_two_students(self):
         journal = Register([])
-        self.assertRaises(TypeError, journal.add, ([2, 3, 4, 5], [2]))
+        self.assertRaises(TypeError, journal.add_new_student, ([2, 3, 4, 5], [2]))
 
     def test_return_error_message_if_add_invalid_type_of_marks(self):
         journal = Register([])
-        self.assertRaises(TypeError, journal.add, ['two', 'three'])
+        self.assertRaises(TypeError, journal.add_new_student, ['two', 'three'])
 
     def test_return_error_message_if_add_negative_marks(self):
         journal = Register([])
-        self.assertRaises(ValueError, journal.add, [-2, 3, -4, 5])
+        self.assertRaises(ValueError, journal.add_new_student, [-2, 3, -4, 5])
 
     def test_return_error_message_if_add_mark_less_1(self):
         journal = Register([])
-        self.assertRaises(ValueError, journal.add, [3, 1, 0, 5])
+        self.assertRaises(ValueError, journal.add_new_student, [3, 1, 0, 5])
 
     def test_return_error_message_if_add_mark_greater_5(self):
         journal = Register([])
-        self.assertRaises(ValueError, journal.add, [4, 3, 7, 5])
+        self.assertRaises(ValueError, journal.add_new_student, [4, 3, 7, 5])
 
 
-class TestNumberOfFailingStudents(unittest.TestCase):
-    def test_number_of_failing_student(self):
-        ivanov = Student([2, 2, 2, 2])
-        smirnov = Student([2, 3, 2, 2, 3])
-        sidorov = Student([4, 5, 4, 5, 5, 5])
-        petrov = Student([2, 3, 2])
+class TestFailingStudents(unittest.TestCase):
+    def test_surnames_of_failing_students(self):
+        ivanov = StudentMarks([2, 2, 2, 2])
+        smirnov = StudentMarks([2, 3, 2, 2, 3])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
+        petrov = StudentMarks([2, 3, 2])
         journal = Register([ivanov, smirnov, sidorov, petrov])
-        result = journal.losers()
+        result = journal.surnames_of_losers()
+        self.assertEqual([ivanov, smirnov, petrov], result)
+
+    def test_number_of_failing_student(self):
+        ivanov = StudentMarks([2, 2, 2, 2])
+        smirnov = StudentMarks([2, 3, 2, 2, 3])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
+        petrov = StudentMarks([2, 3, 2])
+        journal = Register([ivanov, smirnov, sidorov, petrov])
+        result = journal.count_of_losers()
         self.assertEqual(3, result)
 
     def test_number_of_failing_students_among_successful_student(self):
-        ivanov = Student([2, 3, 4, 5])
-        smirnov = Student([4, 3, 4, 4, 5])
-        sidorov = Student([4, 5, 4, 5, 5, 5])
+        ivanov = StudentMarks([2, 3, 4, 5])
+        smirnov = StudentMarks([4, 3, 4, 4, 5])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
         journal = Register([ivanov, smirnov, sidorov])
-        result = journal.losers()
+        result = journal.count_of_losers()
         self.assertEqual(0, result)
 
     def test_number_of_failing_students_not_negative(self):
-        sidorov = Student([4, 5, 4, 5, 5, 5])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
         journal = Register([sidorov])
-        result = journal.losers()
+        result = journal.count_of_losers()
         self.assertGreaterEqual(result, 0)
 
 
-class TestNumberOfSuccessfulStudents(unittest.TestCase):
-    def test_number_of_successful_student(self):
-        ivanov = Student([5, 5, 4, 5, 5])
-        smirnov = Student([2, 3])
-        sidorov = Student([4, 5, 4, 5, 5, 5])
-        petrov = Student([2, 4])
+class TestSuccessfulStudents(unittest.TestCase):
+    def test_surnames_of_successful_students(self):
+        ivanov = StudentMarks([5, 5, 4, 5, 5])
+        smirnov = StudentMarks([2, 3, 5, 5, 5, 5, 5])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
+        petrov = StudentMarks([2, 4])
         journal = Register([ivanov, smirnov, sidorov, petrov])
-        result = journal.success()
+        result = journal.students_who_successfully_pass()
+        self.assertEqual([ivanov, sidorov], result)
+
+    def test_number_of_successful_student(self):
+        ivanov = StudentMarks([5, 5, 4, 5, 5])
+        smirnov = StudentMarks([2, 3])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
+        petrov = StudentMarks([2, 4])
+        journal = Register([ivanov, smirnov, sidorov, petrov])
+        result = journal.count_of_students_who_successfully_pass()
         self.assertEqual(2, result)
 
     def test_number_of_successful_students_among_failing_student(self):
-        ivanov = Student([2, 3, 2])
-        smirnov = Student([2, 2, 2, 2, 5])
-        sidorov = Student([3, 3, 4, 3])
+        ivanov = StudentMarks([2, 3, 2])
+        smirnov = StudentMarks([2, 2, 2, 2, 5])
+        sidorov = StudentMarks([3, 3, 4, 3])
         journal = Register([ivanov, smirnov, sidorov])
-        result = journal.success()
+        result = journal.count_of_students_who_successfully_pass()
         self.assertEqual(0, result)
 
     def test_number_of_success_students_not_negative(self):
-        sidorov = Student([4, 5, 4, 5, 5, 5])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
         journal = Register([sidorov])
-        result = journal.success()
+        result = journal.count_of_students_who_successfully_pass()
         self.assertGreaterEqual(result, 0)
 
 
-class TestNumberOfExcellentStudents(unittest.TestCase):
-    def test_number_of_excellent_student(self):
-        ivanov = Student([5, 5, 4, 5, 5])
-        smirnov = Student([2, 3])
-        sidorov = Student([4, 5, 4, 5, 5, 5])
-        petrov = Student([2, 4])
+class TestExcellentStudents(unittest.TestCase):
+    def test_surnames_of_excellent_students(self):
+        ivanov = StudentMarks([5, 5, 4, 5, 5])
+        smirnov = StudentMarks([2, 3])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
+        petrov = StudentMarks([2, 4])
         journal = Register([ivanov, smirnov, sidorov, petrov])
-        result = journal.excellent()
+        result = journal.surnames_of_excellent_students()
+        self.assertEqual([ivanov, sidorov], result)
+
+    def test_number_of_excellent_student(self):
+        ivanov = StudentMarks([5, 5, 4, 5, 5])
+        smirnov = StudentMarks([2, 3])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
+        petrov = StudentMarks([2, 4])
+        journal = Register([ivanov, smirnov, sidorov, petrov])
+        result = journal.count_of_excellent()
         self.assertEqual(2, result)
 
     def test_number_of_excellent_students_among_failing_student(self):
-        ivanov = Student([2, 3, 2])
-        smirnov = Student([2, 2, 2, 2, 5])
-        sidorov = Student([3, 3, 4, 3])
+        ivanov = StudentMarks([2, 3, 2])
+        smirnov = StudentMarks([2, 2, 2, 2, 5])
+        sidorov = StudentMarks([3, 3, 4, 3])
         journal = Register([ivanov, smirnov, sidorov])
-        result = journal.excellent()
+        result = journal.count_of_excellent()
         self.assertEqual(0, result)
 
     def test_number_of_excellent_students_not_negative(self):
-        sidorov = Student([4, 5, 4, 5, 5, 5])
+        sidorov = StudentMarks([4, 5, 4, 5, 5, 5])
         journal = Register([sidorov])
-        result = journal.excellent()
+        result = journal.count_of_excellent()
         self.assertGreaterEqual(result, 0)
