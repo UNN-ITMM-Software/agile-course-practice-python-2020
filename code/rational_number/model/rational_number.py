07d1ccc9f00ec929f1a14b4d438762a664668edd
AleksandrPanov <sasha1_1997@mail.ru>
2020-11-12 15:05:34 +0300

    check equality rational numbers

--- code/rational_number/model/rational_number.py
+++ code/rational_number/model/rational_number.py
@@ -1,3 +1,7 @@
 class RationalNumber:
     def __init__(self, numerator, denominator):
-        pass
+        self._num = numerator
+        self._den = denominator
+
+    def __eq__(self, other):
+        return self._den == other._den and self._num == other._num
--- code/rational_number/model/test_rational_number.py
+++ code/rational_number/model/test_rational_number.py
@@ -7,3 +7,8 @@ class TestRationalNumberClass(unittest.TestCase):
     def test_can_create_rational_number(self):
         number = RationalNumber(-1, 2)
         self.assertTrue(isinstance(number, RationalNumber))
+
+    def test_can_check_equality_rational_numbers(self):
+        number = RationalNumber(-1, 2)
+        self.assertEquals(number, RationalNumber(-1, 2))
+        self.assertEquals(number, RationalNumber(-1, 2))
