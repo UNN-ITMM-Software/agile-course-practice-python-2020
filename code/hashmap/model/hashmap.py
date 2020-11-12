d2651e6aa31d92991f05a85630b81b31068f9157
LikHait <hait.08@mail.ru>
2020-11-12 20:18:11 +0300

    Implement delete

--- code/hashmap/model/hashmap.py
+++ code/hashmap/model/hashmap.py
@@ -1,18 +1,25 @@
 
 class Hashmap(object):
 
+    def __get_hash(self, key):
+        return hash(key) % self.__size
+
     def __init__(self, size=113):
         self.__size = size
         self.__obj = [None for _ in range(size)]
         pass
 
     def insert(self, key, value):
-        h = hash(key) % self.__size
+        h = self.__get_hash(key)
         self.__obj[h] = (key, value)
 
     def get(self, key):
-        h = hash(key) % self.__size
+        h = self.__get_hash(key)
         return self.__obj[h][1]
 
     def delete(self, key):
-        pass
+        h = self.__get_hash(key)
+        if self.__obj[h] is None or self.__obj[h][0] != key:
+            raise KeyError("No key {} in hashmap".format(key))
+        else:
+            self.__obj[h] = None
--- code/hashmap/model/tests_for_hashmap.py
+++ code/hashmap/model/tests_for_hashmap.py
@@ -5,17 +5,17 @@ from hashmap.model.hashmap import Hashmap
 
 class TestHashmap(unittest.TestCase):
 
-    def test_can_create_hashmap(self):
+    def test_create_hashmap_positive(self):
         Hashmap()
 
-    def test_can_insert_and_get_item(self):
+    def test_insert_and_get_item_positive(self):
         hmap = Hashmap()
         obj = "zero"
         hmap.insert(0, obj)
         res = hmap.get(0)
         self.assertEqual(obj, res)
 
-    def test_can_insert_few_items_and_get_items(self):
+    def test_can_insert_few_items_and_get_items_positive(self):
         hmap = Hashmap()
         key1 = 0
         key2 = 1
@@ -27,3 +27,8 @@ class TestHashmap(unittest.TestCase):
         res2 = hmap.get(key2)
         self.assertEqual(obj1, res1)
         self.assertEqual(obj2, res2)
+
+    def test_delete_item_not_exists_negative(self):
+        hmap = Hashmap()
+        with self.assertRaises(KeyError):
+            hmap.delete(0)
