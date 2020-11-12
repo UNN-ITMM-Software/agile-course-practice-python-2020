34bc5adff79997b72eae3c47e82f194f988bdba3
PanovaElena <alyona-gra98@yandex.ru>
2020-11-12 19:37:49 +0300

    added classmethod

--- code/lines_intersect/model/lines_intersect.py
+++ code/lines_intersect/model/lines_intersect.py
@@ -23,49 +23,49 @@ class Line:
         self.point1 = (float(point1[0]), float(point1[1]))
         self.point2 = (float(point2[0]), float(point2[1]))
 
+    @classmethod
+    def is_intersect(cls, line1, line2):
+        """Determine if two line segments intersect.
 
-def determine_if_two_line_segments_intersect(line_segment_1, line_segment_2):
-    """Determine if two line segments intersect.
+        Keyword arguments:
+        line1 -- the first line segment
+        line2 -- the second line segment
 
-    Keyword arguments:
-    line_segment_1 -- the first line segment
-    line_segment_2 -- the second line segment
+        Returns:
+        True of False
 
-    Returns:
-    True of False
+        A point is a pair of two real numbers (x, y).
+        Two line segments do not intersect if they have no common points.
 
-    A point is a pair of two real numbers (x, y).
-    Two line segments do not intersect if they have no common points.
+        """
+        p11, p12 = (line1.point1, line1.point2)
+        p21, p22 = (line2.point1, line2.point2)
 
-    """
-    p11, p12 = (line_segment_1.point1, line_segment_1.point2)
-    p21, p22 = (line_segment_2.point1, line_segment_2.point2)
+        inv_matrix = np.array([[p21[1]-p22[1], p22[0]-p21[0]],
+                               [p11[1]-p12[1], p12[0]-p11[0]]], dtype=np.float64)
+        det = np.linalg.det(inv_matrix)
 
-    inv_matrix = np.array([[p21[1]-p22[1], p22[0]-p21[0]],
-                           [p11[1]-p12[1], p12[0]-p11[0]]], dtype=np.float64)
-    det = np.linalg.det(inv_matrix)
+        def is_float_eq(a, b):
+            return np.isclose(a, b, rtol=1e-10, atol=1e-12)
 
-    def is_float_eq(a, b):
-        return np.isclose(a, b, rtol=1e-10, atol=1e-12)
+        def is_float_between(a, c1, c2):
+            return is_float_eq(a, c1) or is_float_eq(a, c2) or c1 < a < c2
 
-    def is_float_between(a, c1, c2):
-        return is_float_eq(a, c1) or is_float_eq(a, c2) or c1 < a < c2
+        if is_float_eq(det, 0.0):  # line segments are parallel
+            if not is_float_eq(np.linalg.det(np.array([[p11[0]-p21[0], p11[0]-p12[0]],
+                                                       [p11[1]-p21[1], p11[1]-p12[1]]],
+                                                      dtype=np.float64)), 0.0):
+                return False
 
-    if is_float_eq(det, 0.0):  # line segments are parallel
-        if not is_float_eq(np.linalg.det(np.array([[p11[0]-p21[0], p11[0]-p12[0]],
-                                                   [p11[1]-p21[1], p11[1]-p12[1]]],
-                                                  dtype=np.float64)), 0.0):
-            return False
+            def is_point_on_line(point, line):
+                check_x = is_float_between(point[0], line.point1[0], line.point2[0])
+                check_y = is_float_between(point[1], line.point1[1], line.point2[1])
+                return check_x and check_y
 
-        def is_point_on_line(point, line):
-            check_x = is_float_between(point[0], line.point1[0], line.point2[0])
-            check_y = is_float_between(point[1], line.point1[1], line.point2[1])
-            return check_x and check_y
+            check_line1 = is_point_on_line(p11, line2) or is_point_on_line(p12, line2)
+            check_line2 = is_point_on_line(p21, line1) or is_point_on_line(p22, line1)
+            return check_line1 or check_line2
 
-        check_line1 = is_point_on_line(p11, line_segment_2) or is_point_on_line(p12, line_segment_2)
-        check_line2 = is_point_on_line(p21, line_segment_1) or is_point_on_line(p22, line_segment_1)
-        return check_line1 or check_line2
-
-    vector = np.array([p21[0]-p11[0], p21[1]-p11[1]], dtype=np.float64)
-    params = np.dot(inv_matrix, vector) / det
-    return is_float_between(params[0], 0, 1) and is_float_between(params[1], 0, 1)
+        vector = np.array([p21[0]-p11[0], p21[1]-p11[1]], dtype=np.float64)
+        params = np.dot(inv_matrix, vector) / det
+        return is_float_between(params[0], 0, 1) and is_float_between(params[1], 0, 1)
--- code/lines_intersect/model/test_lines_intersect.py
+++ code/lines_intersect/model/test_lines_intersect.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lines_intersect.model.lines_intersect import determine_if_two_line_segments_intersect, Line
+from lines_intersect.model.lines_intersect import Line
 
 
 class TestLineClass(unittest.TestCase):
@@ -31,78 +31,78 @@ class TestTwoLinesIntersection(unittest.TestCase):
     def test_can_det_if_two_lines_intersect(self):
         line1 = Line((0, 0), (1, 1))
         line2 = Line((0, 1), (1, 0))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_if_two_lines_do_not_intersect(self):
         line1 = Line((0, 0), (0, 1))
         line2 = Line((1, 0), (1, 1))
-        self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertFalse(Line.is_intersect(line1, line2))
 
     def test_can_det_if_two_parallel_lines_do_not_intersect(self):
         line1 = Line((0, 0), (1, 0))
         line2 = Line((0, 1), (1, 1))
-        self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertFalse(Line.is_intersect(line1, line2))
 
     def test_can_det_if_overlapping_lines_intersect(self):
         line1 = Line((0, 0), (1, 0))
         line2 = Line((0.5, 0), (1.5, 0))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_if_equal_lines_intersect(self):
         line1 = Line((0, 0), (1, 0))
         line2 = Line((0, 0), (1, 0))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_if_not_overlapping_parallel_x_lines_do_not_intersect(self):
         line1 = Line((0, 0), (1, 0))
         line2 = Line((2, 0), (3, 0))
-        self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertFalse(Line.is_intersect(line1, line2))
 
     def test_can_det_if_not_overlapping_parallel_y_lines_do_not_intersect(self):
         line1 = Line((0, 0), (0, 1))
         line2 = Line((0, 2), (0, 3))
-        self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertFalse(Line.is_intersect(line1, line2))
 
     def test_can_det_if_not_overlapping_parallel_lines_do_not_intersect(self):
         line1 = Line((0, 0), (1, 1))
         line2 = Line((-2, -2), (-3, -3))
-        self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertFalse(Line.is_intersect(line1, line2))
 
     def test_can_det_if_parallel_lines_with_common_edge_intersect(self):
         line1 = Line((0, 0), (1, 1))
         line2 = Line((1, 1), (3, 3))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_if_unequal_points_do_not_intersect(self):
         point1 = Line((0, 0), (0, 0))
         point2 = Line((0.1, 0.1), (0.1, 0.1))
-        self.assertFalse(determine_if_two_line_segments_intersect(point1, point2))
+        self.assertFalse(Line.is_intersect(point1, point2))
 
     def test_can_det_if_equal_points_intersect(self):
         point = Line((0.1, -0.1), (0.1, -0.1))
-        self.assertTrue(determine_if_two_line_segments_intersect(point, point))
+        self.assertTrue(Line.is_intersect(point, point))
 
     def test_can_det_if_line_segments_with_common_edge_point_intersect(self):
         line1 = Line((0, 0), (0.1, 0.1))
         line2 = Line((0.05, 0.05), (0, 0.1))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_if_line_segments_with_common_edges_intersect(self):
         line1 = Line((0, 0), (0.1, 0.1))
         line2 = Line((0.05, 0), (0.1, 0.1))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_if_point_and_line_segment_intersect_1(self):
         line1 = Line((0, 0), (0, 0))
         line2 = Line((-0.05, -0.05), (0.1, 0.1))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_if_point_and_line_segment_intersect_2(self):
         line1 = Line((-0.05, -0.05), (0.1, 0.1))
         line2 = Line((0, 0), (0, 0))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
 
     def test_can_det_intersection_if_float_numbers_are_large(self):
         line1 = Line((0, 0), (0, 1e10))
         line2 = Line((0, 1e10), (0, 2e10))
-        self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
+        self.assertTrue(Line.is_intersect(line1, line2))
