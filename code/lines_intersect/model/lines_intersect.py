a30aae59a6faa16c70b523f33e3b1eb4ccd5370d
PanovaElena <alyona-gra98@yandex.ru>
2020-11-12 19:03:43 +0300

    added class Line

--- code/lines_intersect/model/lines_intersect.py
+++ code/lines_intersect/model/lines_intersect.py
@@ -1,12 +1,35 @@
 import numpy as np
 
 
+class Line:
+    def __init__(self, point1, point2):
+        """Create a line segment.
+
+        Keyword arguments:
+        point1 -- the first point of line segment
+        point2 -- the second point of line segment
+
+        A point is a tuple of two real numbers (x, y).
+
+        """
+        try:
+            x1, y1 = point1
+            x2, y2 = point2
+            for value in [x1, y1, x2, y2]:
+                if not type(value) in [int, float]:
+                    raise TypeError
+        except:
+            raise TypeError("Point must be a tuple of two real numbers")
+        self.point1 = (float(point1[0]), float(point1[1]))
+        self.point2 = (float(point2[0]), float(point2[1]))
+
+
 def determine_if_two_line_segments_intersect(line_segment_1, line_segment_2):
     """Determine if two line segments intersect.
 
     Keyword arguments:
-    line_segment_1 -- the first line segment (a pair of two points)
-    line_segment_2 -- the second line segment (a pair of two points)
+    line_segment_1 -- the first line segment
+    line_segment_2 -- the second line segment
 
     Returns:
     True of False
@@ -15,8 +38,8 @@ def determine_if_two_line_segments_intersect(line_segment_1, line_segment_2):
     Two line segments do not intersect if they have no common points.
 
     """
-    p11, p12 = line_segment_1
-    p21, p22 = line_segment_2
+    p11, p12 = (line_segment_1.point1, line_segment_1.point2)
+    p21, p22 = (line_segment_2.point1, line_segment_2.point2)
 
     inv_matrix = np.array([[p21[1]-p22[1], p22[0]-p21[0]],
                            [p11[1]-p12[1], p12[0]-p11[0]]], dtype=np.float64)
@@ -35,8 +58,8 @@ def determine_if_two_line_segments_intersect(line_segment_1, line_segment_2):
             return False
 
         def is_point_on_line(point, line):
-            check_x = is_float_between(point[0], line[0][0], line[1][0])
-            check_y = is_float_between(point[1], line[0][1], line[1][1])
+            check_x = is_float_between(point[0], line.point1[0], line.point2[0])
+            check_y = is_float_between(point[1], line.point1[1], line.point2[1])
             return check_x and check_y
 
         check_line1 = is_point_on_line(p11, line_segment_2) or is_point_on_line(p12, line_segment_2)
--- code/lines_intersect/model/test_lines_intersect.py
+++ code/lines_intersect/model/test_lines_intersect.py
@@ -1,84 +1,108 @@
 import unittest
 
-from lines_intersect.model.lines_intersect import determine_if_two_line_segments_intersect
+from lines_intersect.model.lines_intersect import determine_if_two_line_segments_intersect, Line
 
 
-class TestFractionClass(unittest.TestCase):
+class TestLineClass(unittest.TestCase):
+    def test_can_not_create_line_if_point_is_not_a_pair(self):
+        point1 = (1, 2)
+        point2 = (1, 2, 3)
+        self.assertRaises(TypeError, Line, point1, point2)
+
+    def test_can_not_create_line_if_coordinate_has_not_a_correct_type(self):
+        point1 = ("1", 2)
+        point2 = (1, 2.0)
+        self.assertRaises(TypeError, Line, point1, point2)
+
+    def test_can_create_line_if_args_are_correct(self):
+        point1 = (3.0, 2.0)
+        point2 = (1.0, 2.0)
+        line = Line(point1, point2)
+        self.assertTrue(point1 == line.point1 and point2 == line.point2)
+
+    def test_points_of_created_line_are_float_numbers(self):
+        point1 = (3.0, 2.0)
+        point2 = (1, 2.0)
+        line = Line(point1, point2)
+        self.assertIsInstance(line.point2[0], float)
+
+
+class TestTwoLinesIntersection(unittest.TestCase):
     def test_can_det_if_two_lines_intersect(self):
-        line1 = ((0, 0), (1, 1))
-        line2 = ((0, 1), (1, 0))
+        line1 = Line((0, 0), (1, 1))
+        line2 = Line((0, 1), (1, 0))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_two_lines_do_not_intersect(self):
-        line1 = ((0, 0), (0, 1))
-        line2 = ((1, 0), (1, 1))
+        line1 = Line((0, 0), (0, 1))
+        line2 = Line((1, 0), (1, 1))
         self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_two_parallel_lines_do_not_intersect(self):
-        line1 = ((0, 0), (1, 0))
-        line2 = ((0, 1), (1, 1))
+        line1 = Line((0, 0), (1, 0))
+        line2 = Line((0, 1), (1, 1))
         self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_overlapping_lines_intersect(self):
-        line1 = ((0, 0), (1, 0))
-        line2 = ((0.5, 0), (1.5, 0))
+        line1 = Line((0, 0), (1, 0))
+        line2 = Line((0.5, 0), (1.5, 0))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_equal_lines_intersect(self):
-        line1 = ((0, 0), (1, 0))
-        line2 = ((0, 0), (1, 0))
+        line1 = Line((0, 0), (1, 0))
+        line2 = Line((0, 0), (1, 0))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_not_overlapping_parallel_x_lines_do_not_intersect(self):
-        line1 = ((0, 0), (1, 0))
-        line2 = ((2, 0), (3, 0))
+        line1 = Line((0, 0), (1, 0))
+        line2 = Line((2, 0), (3, 0))
         self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_not_overlapping_parallel_y_lines_do_not_intersect(self):
-        line1 = ((0, 0), (0, 1))
-        line2 = ((0, 2), (0, 3))
+        line1 = Line((0, 0), (0, 1))
+        line2 = Line((0, 2), (0, 3))
         self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_not_overlapping_parallel_lines_do_not_intersect(self):
-        line1 = ((0, 0), (1, 1))
-        line2 = ((-2, -2), (-3, -3))
+        line1 = Line((0, 0), (1, 1))
+        line2 = Line((-2, -2), (-3, -3))
         self.assertFalse(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_parallel_lines_with_common_edge_intersect(self):
-        line1 = ((0, 0), (1, 1))
-        line2 = ((1, 1), (3, 3))
+        line1 = Line((0, 0), (1, 1))
+        line2 = Line((1, 1), (3, 3))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_unequal_points_do_not_intersect(self):
-        point1 = ((0, 0), (0, 0))
-        point2 = ((0.1, 0.1), (0.1, 0.1))
+        point1 = Line((0, 0), (0, 0))
+        point2 = Line((0.1, 0.1), (0.1, 0.1))
         self.assertFalse(determine_if_two_line_segments_intersect(point1, point2))
 
     def test_can_det_if_equal_points_intersect(self):
-        point = ((0.1, -0.1), (0.1, -0.1))
+        point = Line((0.1, -0.1), (0.1, -0.1))
         self.assertTrue(determine_if_two_line_segments_intersect(point, point))
 
     def test_can_det_if_line_segments_with_common_edge_point_intersect(self):
-        line1 = ((0, 0), (0.1, 0.1))
-        line2 = ((0.05, 0.05), (0, 0.1))
+        line1 = Line((0, 0), (0.1, 0.1))
+        line2 = Line((0.05, 0.05), (0, 0.1))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_line_segments_with_common_edges_intersect(self):
-        line1 = ((0, 0), (0.1, 0.1))
-        line2 = ((0.05, 0), (0.1, 0.1))
+        line1 = Line((0, 0), (0.1, 0.1))
+        line2 = Line((0.05, 0), (0.1, 0.1))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_point_and_line_segment_intersect_1(self):
-        line1 = ((0, 0), (0, 0))
-        line2 = ((-0.05, -0.05), (0.1, 0.1))
+        line1 = Line((0, 0), (0, 0))
+        line2 = Line((-0.05, -0.05), (0.1, 0.1))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_if_point_and_line_segment_intersect_2(self):
-        line1 = ((-0.05, -0.05), (0.1, 0.1))
-        line2 = ((0, 0), (0, 0))
+        line1 = Line((-0.05, -0.05), (0.1, 0.1))
+        line2 = Line((0, 0), (0, 0))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
 
     def test_can_det_intersection_if_float_numbers_are_large(self):
-        line1 = ((0, 0), (0, 1e10))
-        line2 = ((0, 1e10), (0, 2e10))
+        line1 = Line((0, 0), (0, 1e10))
+        line2 = Line((0, 1e10), (0, 2e10))
         self.assertTrue(determine_if_two_line_segments_intersect(line1, line2))
