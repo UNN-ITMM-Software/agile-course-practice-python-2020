672db930eeec3bd631c2ea2724f5a0eb0e19b1dd
Aleksandr <aleksandr-generalov19@yandex.ru>
2020-11-12 22:39:54 +0300

    fix issues

--- code/vector_3d/__init__.py
+++
@@ -1,6 +0,0 @@
-# uncompyle6 version 3.7.4
-# Python bytecode 3.7 (3394)
-# Decompiled from: Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)]
-# Embedded file name: D:\local\agile-course-practice-python\code\vector_3d\__init__.py
-# Compiled at: 2020-11-10 22:09:07
-pass
