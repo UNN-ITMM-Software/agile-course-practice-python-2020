a6ea0a98182b1a170e58e141863dee1957a21e11
Andrey Kalistratov <Farvata@mail.ru>
2020-11-13 11:57:51 +0300

    removed string handling

--- code/game_of_life/game_of_life.py
+++ code/game_of_life/game_of_life.py
@@ -1,15 +1,17 @@
 class GameOfLife(object):
-    def start(self, string_field):
-        if string_field == "":
-            return ""
-        parser = GameOfLifeParser()
-        field = parser.parse(string_field)
-        field_boundaries = self.calc_field_boundaries(field)
-        next_field = self.step_calculation(field, field_boundaries)
-        res_string = self.convert_to_string(next_field)
-        return res_string
+    def start(self, field):
+        self.__string_validation(field)
+        field_boundaries = self.__calc_field_boundaries(field)
+        next_field = self.__step_calculation(field, field_boundaries)
+        return next_field
 
-    def calc_field_boundaries(self, current_field):
+    def __string_validation(self, field):
+        for field_line in field:
+            if len(field[0]) != len(field_line):
+                msg = "invalid string entered"
+                raise ValueError(msg)
+
+    def __calc_field_boundaries(self, current_field):
         field_boundaries = {}
         height_field = len(current_field)
         width_field = len(current_field[0])
@@ -17,44 +19,44 @@ class GameOfLife(object):
         field_boundaries["width_field"] = width_field
         return field_boundaries
 
-    def create_empty_field(self, field_boundaries):
-        next_field = [[0 for i in range(field_boundaries["width_field"])]
-                      for j in range(field_boundaries["height_field"])]
-        return next_field
+    def __create_empty_field(self, field_boundaries):
+        empty_field = [[0 for i in range(field_boundaries["width_field"])]
+                       for j in range(field_boundaries["height_field"])]
+        return empty_field
 
-    def step_calculation(self, current_field, field_boundaries):
-        next_field = self.create_empty_field(field_boundaries)
+    def __step_calculation(self, current_field, field_boundaries):
+        next_field = self.__create_empty_field(field_boundaries)
         for i in range(field_boundaries["height_field"]):
             for j in range(field_boundaries["width_field"]):
-                current_neighbors = self.counting_neighbors(current_field, i, j)
-                if current_field[i][j]:
+                current_neighbors = self.__counting_neighbors(current_field, i, j)
+                if current_field[i][j] == 1:
                     if current_neighbors == 2 or current_neighbors == 3:
                         next_field[i][j] = 1
                     else:
                         next_field[i][j] = 0
-                else:
+                elif current_field[i][j] == 0:
                     if current_neighbors == 3:
                         next_field[i][j] = 1
         return next_field
 
-    def counting_neighbors(self, current_field, y, x):
+    def __counting_neighbors(self, current_field, y, x):
         count = 0
-        step = self.calc_border_of_neighbors(current_field, y, x)
+        step = self.__calc_border_of_neighbors(current_field, y, x)
         for i in range(y - step["step_back_y"], y + step["step_forward_y"] + 1):
             for j in range(x - step["step_back_x"], x + step["step_forward_x"] + 1):
                 if current_field[i][j]:
                     count += 1
-        if current_field[y][x]:
+        if current_field[y][x] == 1:
             count -= 1
         return count
 
-    def calc_border_of_neighbors(self, current_field, y, x):
+    def __calc_border_of_neighbors(self, current_field, y, x):
         step = {}
         step_back_y = 1
         step_back_x = 1
         step_forward_y = 1
         step_forward_x = 1
-        field_boundaries = self.calc_field_boundaries(current_field)
+        field_boundaries = self.__calc_field_boundaries(current_field)
         if y == 0:
             step_back_y = 0
         if x == 0:
@@ -68,52 +70,3 @@ class GameOfLife(object):
         step["step_forward_y"] = step_forward_y
         step["step_forward_x"] = step_forward_x
         return step
-
-    def convert_to_string(self, current_field):
-        field_boundaries = self.calc_field_boundaries(current_field)
-        string = ""
-        for i in range(field_boundaries["height_field"]):
-            for j in range(field_boundaries["width_field"]):
-                if current_field[i][j]:
-                    string += "*"
-                else:
-                    string += "."
-            if i != field_boundaries["height_field"] - 1:
-                string += "\n"
-        return string
-
-
-class GameOfLifeParser(object):
-    def parse(self, string):
-        field_lines = string.split()
-        field_boundaries = self.calc_filed_boundaries(field_lines)
-        next_field = self.create_empty_field(field_boundaries)
-        for i in range(field_boundaries["height_field"]):
-            for j in range(field_boundaries["width_field"]):
-                if field_lines[i][j] == '.':
-                    next_field[i][j] = 0
-                elif field_lines[i][j] == '*':
-                    next_field[i][j] = 1
-                else:
-                        msg = "invalid character entered" + ', ' + field_lines[i][j]
-                        raise ValueError(msg)
-        return next_field
-
-    def string_validation(self, field_lines):
-        for field_line in field_lines:
-            if len(field_lines[0]) != len(field_line):
-                msg = "invalid string entered"
-                raise ValueError(msg)
-
-    def calc_filed_boundaries(self, field_lines):
-        field_boundaries = {}
-        height_field = len(field_lines)
-        width_field = len(field_lines[0])
-        field_boundaries["height_field"] = height_field
-        field_boundaries["width_field"] = width_field
-        return field_boundaries
-
-    def create_empty_field(self, field_boundaries):
-        next_field = [[0 for i in range(field_boundaries["width_field"])]
-                      for j in range(field_boundaries["height_field"])]
-        return next_field
--- code/game_of_life/game_of_life_test.py
+++ code/game_of_life/game_of_life_test.py
@@ -1,215 +1,89 @@
 import unittest
 
 from game_of_life import GameOfLife
-from game_of_life import GameOfLifeParser
 
 
 class TestGameOfLifeClass(unittest.TestCase):
-    def test_start_empty_string_is_0(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("")
-        self.assertTrue(result == "")
-
     def test_start_2x2_all_points_are_dead(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("..\n..")
-        field_test = "..\n.."
+        game = GameOfLife()
+        field_test = [[0, 0],
+                      [0, 0]]
+        result = game.start(field_test)
         self.assertTrue(result == field_test)
 
     def test_start_2x2_first_point_live(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("*.\n..")
-        field_test = "..\n.."
-        self.assertTrue(result == field_test)
+        game = GameOfLife()
+        field_test = [[1, 0],
+                      [0, 0]]
+        result = game.start(field_test)
+        correct_field = [[0, 0],
+                         [0, 0]]
+        self.assertTrue(result == correct_field)
 
     def test_start_2x2_two_points_on_the_first_line_are_live(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("**\n..")
-        field_test = "..\n.."
-        self.assertTrue(result == field_test)
+        game = GameOfLife()
+        field_test = [[1, 1],
+                      [0, 0]]
+        result = game.start(field_test)
+        correct_field = [[0, 0],
+                         [0, 0]]
+        self.assertTrue(result == correct_field)
 
     def test_start_2x2_last_point_is_dead(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("**\n*.")
-        field_test = "**\n**"
-        self.assertTrue(result == field_test)
+        game = GameOfLife()
+        field_test = [[1, 1],
+                      [1, 0]]
+        result = game.start(field_test)
+        correct_field = [[1, 1],
+                         [1, 1]]
+        self.assertTrue(result == correct_field)
 
     def test_start_3x3_living_square(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("**.\n*..\n...")
-        field_test = "**.\n**.\n..."
-        self.assertTrue(result == field_test)
+        game = GameOfLife()
+        field_test = [[1, 1, 0],
+                      [1, 0, 0],
+                      [0, 0, 0]]
+        result = game.start(field_test)
+        correct_field = [[1, 1, 0],
+                         [1, 1, 0],
+                         [0, 0, 0]]
+        self.assertTrue(result == correct_field)
 
     def test_start_4x8_general_example(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("........\n....*...\n...**...\n........")
-        field_test = "........\n...**...\n...**...\n........"
-        self.assertTrue(result == field_test)
+        game = GameOfLife()
+        field_test = [[0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 1, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = game.start(field_test)
+        correct_field = [[0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 1, 0, 0, 0],
+                         [0, 0, 0, 1, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0]]
+        self.assertTrue(result == correct_field)
 
     def test_start_4x8_line(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("........\n...***..\n........\n........")
-        field_test = "....*...\n....*...\n....*...\n........"
-        self.assertTrue(result == field_test)
+        game = GameOfLife()
+        field_test = [[0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 1, 1, 1, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = game.start(field_test)
+        correct_field = [[0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0]]
+        self.assertTrue(result == correct_field)
 
     def test_start_4x8_column(self):
-        strcalc = GameOfLife()
-        result = strcalc.start("....*...\n....*...\n....*...\n........")
-        field_test = "........\n...***..\n........\n........"
-        self.assertTrue(result == field_test)
-
-    def test_counting_neighbors_0(self):
-        parser = GameOfLifeParser()
-        game_of_life = GameOfLife()
-        field = parser.parse("........\n....*...\n........\n........")
-        result = game_of_life.counting_neighbors(field, 1, 4)
-        self.assertTrue(result == 0)
-
-    def test_counting_neighbors_8(self):
-        parser = GameOfLifeParser()
-        game_of_life = GameOfLife()
-        field = parser.parse("...***..\n...***..\n...***..\n........")
-        result = game_of_life.counting_neighbors(field, 1, 4)
-        self.assertTrue(result == 8)
-
-    def test_counting_neighbors_5(self):
-        parser = GameOfLifeParser()
-        game_of_life = GameOfLife()
-        field = parser.parse("...***..\n...***..\n...***..\n........")
-        result = game_of_life.counting_neighbors(field, 1, 3)
-        self.assertTrue(result == 5)
-
-    def test_counting_neighbors_3_x_0_y_0(self):
-        parser = GameOfLifeParser()
-        game_of_life = GameOfLife()
-        field = parser.parse(".*......\n**......\n........\n........")
-        result = game_of_life.counting_neighbors(field, 0, 0)
-        self.assertTrue(result == 3)
-
-    def test_counting_neighbors_3_x_8_y_4(self):
-        parser = GameOfLifeParser()
-        game_of_life = GameOfLife()
-        field = parser.parse("........\n........\n......**\n......**")
-        result = game_of_life.counting_neighbors(field, 3, 7)
-        self.assertTrue(result == 3)
-
-    def test_convert_to_string_2x2_all_points_are_dead(self):
-        converter = GameOfLife()
-        field_test = [[0 for i in range(2)] for j in range(2)]
-        result = converter.convert_to_string(field_test)
-        self.assertTrue(result == "..\n..")
-
-    def test_convert_to_string_2x2_first_point_live(self):
-        converter = GameOfLife()
-        field_test = [[0 for i in range(2)] for j in range(2)]
-        field_test[0][0] = 1
-        result = converter.convert_to_string(field_test)
-        self.assertTrue(result == "*.\n..")
-
-    def test_convert_to_string_2x2_second_point_live(self):
-        converter = GameOfLife()
-        field_test = [[0 for i in range(2)] for j in range(2)]
-        field_test[0][1] = 1
-        result = converter.convert_to_string(field_test)
-        self.assertTrue(result == ".*\n..")
-
-    def test_convert_to_string_2x2_two_points_second_column_live(self):
-        converter = GameOfLife()
-        field_test = [[0 for i in range(2)] for j in range(2)]
-        field_test[0][1] = 1
-        field_test[1][1] = 1
-        result = converter.convert_to_string(field_test)
-        self.assertTrue(result == ".*\n.*")
-
-    def test_convert_to_string_4x4_last_point_live(self):
-        converter = GameOfLife()
-        field_test = [[0 for i in range(4)] for j in range(4)]
-        field_test[3][3] = 1
-        result = converter.convert_to_string(field_test)
-        self.assertTrue(result == "....\n....\n....\n...*")
-
-    def test_convert_to_string_4x8_general_example(self):
-        converter = GameOfLife()
-        field_test = [[0 for i in range(8)] for j in range(4)]
-        field_test[1][4] = 1
-        field_test[2][3] = 1
-        field_test[2][4] = 1
-        result = converter.convert_to_string(field_test)
-        self.assertTrue(result == "........\n....*...\n...**...\n........")
-
-
-class TestGameOfLifeParserClass(unittest.TestCase):
-    def test_can_create_parser(self):
-        parser = GameOfLifeParser()
-        self.assertTrue(isinstance(parser, GameOfLifeParser))
-
-    def test_parse_check_string_2x2_all_points_are_dead(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(2)] for j in range(2)]
-        result = parser.parse("..\n..")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_2x2_first_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(2)] for j in range(2)]
-        field_test[0][0] = 1
-        result = parser.parse("*.\n..")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_2x2_second_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(2)] for j in range(2)]
-        field_test[0][1] = 1
-        result = parser.parse(".*\n..")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_3x3_second_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(3)] for j in range(3)]
-        field_test[0][1] = 1
-        result = parser.parse(".*.\n...\n...")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_3x2_second_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(2)] for j in range(3)]
-        field_test[0][1] = 1
-        result = parser.parse(".*\n..\n..")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_3x3_second_line_second_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(3)] for j in range(3)]
-        field_test[1][1] = 1
-        result = parser.parse("...\n.*.\n...")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_3x2_second_line_second_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(2)] for j in range(3)]
-        field_test[1][1] = 1
-        result = parser.parse("..\n.*\n..")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_4x4_last_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(4)] for j in range(4)]
-        field_test[3][3] = 1
-        result = parser.parse("....\n....\n....\n...*")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_3x2_last_point_live(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(3)] for j in range(4)]
-        field_test[3][2] = 1
-        result = parser.parse("...\n...\n...\n..*")
-        self.assertTrue(result == field_test)
-
-    def test_parse_check_string_4x8_general_example(self):
-        parser = GameOfLifeParser()
-        field_test = [[0 for i in range(8)] for j in range(4)]
-        field_test[1][4] = 1
-        field_test[2][3] = 1
-        field_test[2][4] = 1
-        result = parser.parse("........\n....*...\n...**...\n........")
-        self.assertTrue(result == field_test)
+        game = GameOfLife()
+        field_test = [[0, 0, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = game.start(field_test)
+        correct_field = [[0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 1, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0]]
+        self.assertTrue(result == correct_field)
