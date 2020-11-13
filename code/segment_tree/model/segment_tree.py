21d58b874c84a6b39f38ddf24cde870cbedbf6be
Rodionov Fedor <fed.rodionov@gmail.com>
2020-11-13 13:21:33 +0300

    Fix review comments

--- code/segment_tree/model/segment_tree.py
+++ code/segment_tree/model/segment_tree.py
@@ -5,8 +5,14 @@ import math
 class SegmentTree(object):
     def __init__(self, tree_type):
         if tree_type not in ['max', 'min', 'sum']:
-            raise TypeError('Incorrect type of segment tree. Expected: max\\min\\sum')
-        self.type = tree_type
+            raise TypeError(
+                'Incorrect type of segment tree. Expected: max\\min\\sum')
+        self.config = {
+            'sum': (sum, 0),
+            'min': (min, math.inf),
+            'max': (max, -math.inf)
+        }
+        self.operation, self.dafault_value = self.config[tree_type]
 
     def _build_tree(self, vertex, left, right):
         if left == right:
@@ -19,12 +25,7 @@ class SegmentTree(object):
             left_child = self.tree[vertex * 2]
             right_child = self.tree[vertex * 2 + 1]
 
-            if self.type == 'sum':
-                self.tree[vertex] = left_child + right_child
-            elif self.type == 'min':
-                self.tree[vertex] = min(left_child, right_child)
-            elif self.type == 'max':
-                self.tree[vertex] = max(left_child, right_child)
+            self.tree[vertex] = self.operation([left_child, right_child])
 
     def build(self, array):
         if not isinstance(array, list):
@@ -32,12 +33,13 @@ class SegmentTree(object):
 
         for element in array:
             if not isinstance(element, int):
-                raise TypeError('Incorrect type of input array\'s elements. Expected: int')
+                raise TypeError(
+                    'Incorrect type of input array\'s elements. Expected: int')
 
         self.array = copy.deepcopy(array)
         self.size = len(array)
 
-        self.tree = [0] * self.size * 4
+        self.tree = [self.dafault_value] * self.size * 4
 
         self._build_tree(1, 0, self.size - 1)
 
@@ -46,24 +48,14 @@ class SegmentTree(object):
             return self.tree[vertex]
 
         if end < left or right < begin:
-            if self.type == 'sum':
-                return 0
-            elif self.type == 'min':
-                return math.inf
-            elif self.type == 'max':
-                return -math.inf
+            return self.dafault_value
 
         mid = (begin + end) // 2
 
         left_child = self._get(left, right, vertex * 2, begin, mid)
         right_child = self._get(left, right, vertex * 2 + 1, mid + 1, end)
 
-        if self.type == 'sum':
-            return left_child + right_child
-        elif self.type == 'min':
-            return min(left_child, right_child)
-        elif self.type == 'max':
-            return max(left_child, right_child)
+        return self.operation([left_child, right_child])
 
     def get(self, left, right):
         if right < left:
@@ -91,12 +83,7 @@ class SegmentTree(object):
         left_child = self.tree[vertex * 2]
         right_child = self.tree[vertex * 2 + 1]
 
-        if self.type == 'sum':
-            self.tree[vertex] = left_child + right_child
-        elif self.type == 'min':
-            self.tree[vertex] = min(left_child, right_child)
-        elif self.type == 'max':
-            self.tree[vertex] = max(left_child, right_child)
+        self.tree[vertex] = self.operation([left_child, right_child])
 
     def update(self, index, new_value):
         if index + 1 > self.size or index < 0:
--- code/segment_tree/model/test_segment_tree.py
+++ code/segment_tree/model/test_segment_tree.py
@@ -12,117 +12,117 @@ class TestSegmentTree(unittest.TestCase):
         with self.assertRaises(TypeError):
             array = {1: 2, 3: 4}
 
-            st = SegmentTree('sum')
-            st.build(array)
+            tree = SegmentTree('sum')
+            tree.build(array)
 
     def test_can_create_tree_only_with_list_of_integer(self):
         with self.assertRaises(TypeError):
             array = ['solo', 3, 2, 2]
 
-            st = SegmentTree('sum')
-            st.build(array)
+            tree = SegmentTree('sum')
+            tree.build(array)
 
     def test_sum_1(self):
         array = [4, 3, 2, 3, 4]
 
-        st = SegmentTree('sum')
-        st.build(array)
+        tree = SegmentTree('sum')
+        tree.build(array)
 
-        self.assertEqual(st.get(0, 4), sum(array))
+        self.assertEqual(tree.get(0, 4), sum(array))
 
     def test_sum_2(self):
         array = [4, 3, 2, 3, 4]
 
-        st = SegmentTree('sum')
-        st.build(array)
+        tree = SegmentTree('sum')
+        tree.build(array)
 
-        self.assertEqual(st.get(1, 3), sum(array[1:4]))
+        self.assertEqual(tree.get(1, 3), sum(array[1:4]))
 
     def test_exception_if_right_index_is_lower_then_left(self):
         array = [4, 3, 2, 3, 4]
 
-        st = SegmentTree('sum')
-        st.build(array)
+        tree = SegmentTree('sum')
+        tree.build(array)
 
         with self.assertRaises(IndexError):
-            st.get(2, 0)
+            tree.get(2, 0)
 
-    def test_exception_if_index_is_greater_array_size(self):
+    def test_exception_if_index_is_greater_then_array_size(self):
         array = [4, 3, 2, 3, 4]
 
-        st = SegmentTree('sum')
-        st.build(array)
+        tree = SegmentTree('sum')
+        tree.build(array)
 
         with self.assertRaises(IndexError):
-            st.get(0, 8)
+            tree.get(0, 8)
 
     def test_sum_after_update(self):
         array = [1, 3, 2, 5, 6]
 
-        st = SegmentTree('sum')
-        st.build(array)
+        tree = SegmentTree('sum')
+        tree.build(array)
 
-        st.update(index=1, new_value=0)
+        tree.update(index=1, new_value=0)
 
-        self.assertEqual(st.get(0, 4), sum(array) - 3)
+        self.assertEqual(tree.get(0, 4), sum(array) - 3)
 
     def test_exception_if_index_in_update_is_out_of_bounds(self):
         array = [1, 3, 2, 5, 6]
 
-        st = SegmentTree('sum')
-        st.build(array)
+        tree = SegmentTree('sum')
+        tree.build(array)
 
         with self.assertRaises(IndexError):
-            st.update(index=22, new_value=0)
+            tree.update(index=22, new_value=0)
 
     def test_max_1(self):
         array = [1, 0, 8]
 
-        st = SegmentTree('max')
-        st.build(array)
+        tree = SegmentTree('max')
+        tree.build(array)
 
-        self.assertEqual(st.get(0, 2), 8)
+        self.assertEqual(tree.get(0, 2), 8)
 
     def test_max_2(self):
         array = [13, 5, 22, 5, 19, 4]
 
-        st = SegmentTree('max')
-        st.build(array)
+        tree = SegmentTree('max')
+        tree.build(array)
 
-        self.assertEqual(st.get(3, 4), 19)
+        self.assertEqual(tree.get(3, 4), 19)
 
     def test_max_with_update(self):
         array = [1, 0, 8]
 
-        st = SegmentTree('max')
-        st.build(array)
+        tree = SegmentTree('max')
+        tree.build(array)
 
-        st.update(index=1, new_value=15)
+        tree.update(index=1, new_value=15)
 
-        self.assertEqual(st.get(0, 2), 15)
+        self.assertEqual(tree.get(0, 2), 15)
 
     def test_min_1(self):
         array = [1, 0, 8]
 
-        st = SegmentTree('min')
-        st.build(array)
+        tree = SegmentTree('min')
+        tree.build(array)
 
-        self.assertEqual(st.get(0, 2), 0)
+        self.assertEqual(tree.get(0, 2), 0)
 
     def test_min_2(self):
         array = [13, 5, 22, 5, 19, 4]
 
-        st = SegmentTree('min')
-        st.build(array)
+        tree = SegmentTree('min')
+        tree.build(array)
 
-        self.assertEqual(st.get(3, 5), 4)
+        self.assertEqual(tree.get(3, 5), 4)
 
     def test_min_with_update(self):
         array = [13, 5, 22, 5, 19, 4]
 
-        st = SegmentTree('min')
-        st.build(array)
+        tree = SegmentTree('min')
+        tree.build(array)
 
-        st.update(index=2, new_value=0)
+        tree.update(index=2, new_value=0)
 
-        self.assertEqual(st.get(2, 5), 0)
+        self.assertEqual(tree.get(2, 5), 0)
