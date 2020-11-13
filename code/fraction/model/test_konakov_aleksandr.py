266080d4cb043c6d13145bce1763b6d10d0f00e7
Александр <konakov@my.com>
2020-11-13 22:01:05 +0300

    Подправил оформление

--- code/fraction/model/test_konakov_aleksandr.py
+++ code/fraction/model/test_konakov_aleksandr.py
@@ -2,13 +2,14 @@ import unittest
 
 from fraction.model.fraction import Fraction
 
-class TestFraction(unittest.TestCase):
-	def test_fraction_mul(self):
-		f1 = Fraction(1, 3)
-		f2 = Fraction(3, 2)
-		m = f1 * f2
-		self.assertTrue(m.is_equal(1, 2))
 
-	def test_fraction_reduce(self):
-		f = Fraction(2, 10)
-		self.assertTrue(f.is_equal(1, 5))
+class TestFraction(unittest.TestCase):
+    def test_fraction_mul(self):
+        f1 = Fraction(1, 3)
+        f2 = Fraction(3, 2)
+        m = f1 * f2
+        self.assertTrue(m.is_equal(1, 2))
+
+    def test_fraction_reduce(self):
+        f = Fraction(2, 10)
+        self.assertTrue(f.is_equal(1, 5))
