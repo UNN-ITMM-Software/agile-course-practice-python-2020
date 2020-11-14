23843876af6772f5b3c4f440a42d3d18f69b6c9f
Marat Israfilov <marat.israfilov.97@gmail.com>
2020-11-14 20:46:24 +0300

    Added test placeholders

--- code/bit_array/model/test_bit_array.py
+++ code/bit_array/model/test_bit_array.py
@@ -2,5 +2,121 @@ import unittest
 
 from bit_array.model.bit_array import BitArray
 
+
 class TestBitArray(unittest.TestCase):
-    pass
+    def test_empty_bit_array(self):
+        pass
+
+    def test_zero_bit_array(self):
+        pass
+
+    def test_negative_count_of_bits(self):
+        pass
+
+    def test_not_integer_count_of_bits(self):
+        pass
+
+    def test_get_mem_length_of_array(self):
+        pass
+
+    def test_get_length_of_array(self):
+        pass
+
+    def test_get_mask_for_invalid_argument(self):
+        pass
+
+    def test_get_mask_for_out_of_range_argument(self):
+        pass
+
+    def test_get_mask_for_valid_argument(self):
+        pass
+
+    def test_get_array_index_for_invalid_argument(self):
+        pass
+
+    def test_get_array_index_for_out_of_range_argument(self):
+        pass
+
+    def test_get_array_index_for_valid_bit(self):
+        pass
+
+    def test_cant_conver_negative_decimal_to_binary_string(self):
+        pass
+
+    def test_conver_decimal_to_binary_string(self):
+        pass
+
+    def test_set_bit_with_invalid_argument(self):
+        pass
+
+    def test_set_bit_with_out_of_range_argument(self):
+        pass
+
+    def test_set_bit_with_valid_argument(self):
+        pass
+
+    def test_get_bit_with_invalid_argument(self):
+        pass
+
+    def test_get_bit_with_out_of_range_argument(self):
+        pass
+
+    def test_get_bit_with_valid_argument(self):
+        pass
+
+    def test_clear_bit_with_invalid_argument(self):
+        pass
+
+    def test_clear_bit_with_out_of_range_argument(self):
+        pass
+
+    def test_clear_bit_with_valid_argument(self):
+        pass
+
+    def test_convert_array_to_string(self):
+        pass
+
+    def test_or_operation_with_not_bit_array(self):
+        pass
+
+    def test_or_operation_with_equal_size_arrays(self):
+        pass
+
+    def test_or_operation_with_not_equal_size_arrays(self):
+        pass
+
+    def test_and_operation_with_not_bit_array(self):
+        pass
+
+    def test_and_operation_with_equal_size_arrays(self):
+        pass
+
+    def test_and_operation_with_not_equal_size_arrays(self):
+        pass
+
+    def test_xor_operation_with_not_bit_array(self):
+        pass
+
+    def test_xor_operation_with_equal_size_arrays(self):
+        pass
+
+    def test_xor_operation_with_not_equal_size_arrays(self):
+        pass
+
+    def test_inversion(self):
+        pass
+
+    def test_eq_operation_with_not_bit_array(self):
+        pass
+
+    def test_eq_operation_with_equal_size_arrays(self):
+        pass
+
+    def test_eq_operation_with_not_equal_size_arrays(self):
+        pass
+
+    def test_ne_operation_with_not_bit_array(self):
+        pass
+
+    def test_ne_operation(self):
+        pass
