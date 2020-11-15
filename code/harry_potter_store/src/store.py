0286667438b05907c64117c9b0613bee84f7ad67
Daniil Roman <d.roman@just-ai.com>
2020-11-15 13:58:43 +0300

    [lab1] refactor and getting rid of arguments (predicate and min_value)

--- code/harry_potter_store/src/store.py
+++ code/harry_potter_store/src/store.py
@@ -26,11 +26,11 @@ class Store:
         return self._clean(first_part)
 
     def _discount_wrapper(self, number, discount):
-        return lambda to_buy_books, price, predicate: \
-            self._discount(to_buy_books, price, predicate, number, discount)
+        return lambda to_buy_books, price: \
+            self._discount(to_buy_books, price, number, discount)
 
-    def _discount(self, to_buy_books, price, predicate, number, discount):
-        if predicate(len(to_buy_books), number):
+    def _discount(self, to_buy_books, price, number, discount):
+        if len(to_buy_books) >= number:
             price += number * self.book_price * (1 - discount)
             to_buy_books = self._decrement(to_buy_books, number)
         return to_buy_books, price
@@ -52,34 +52,33 @@ class Store:
         """
         self._validate(to_buy_books)
         to_buy_books = {k: v for k, v in sorted(to_buy_books.items(), key=lambda item: item[1], reverse=True)}
-        discounts = [self._discount_wrapper(5, 0.25), self._discount_wrapper(4, 0.2),
-                     self._discount_wrapper(3, 0.1), self._discount_wrapper(2, 0.05)]
+        discounts = {2: self._discount_wrapper(2, 0.05), 3: self._discount_wrapper(3, 0.1),
+                     4: self._discount_wrapper(4, 0.2), 5: self._discount_wrapper(5, 0.25)}
         min_price = self.book_price * sum(to_buy_books.values())
         for i in range(len(discounts)):
+            ith_discount_as_list = list(discounts.items())[i]
+            ith_discount_as_dict = {ith_discount_as_list[0]: ith_discount_as_list[1]}
             min_price = min(min_price,
-                            self._get_price(to_buy_books,
-                                            [discounts[i]], len(discounts) - i, lambda x, y: x >= y))
-            if i != 0:
+                            self._get_price(to_buy_books, ith_discount_as_dict))
+            if i > 1:
                 min_price = min(min_price,
                                 self._get_price(to_buy_books,
-                                                discounts[0:i + 1], len(discounts) - i, lambda x, y: x == y))
+                                                dict(list(discounts.items())[0:i + 1][::-1])))
         return min_price
 
     def _get_price(self, to_buy_books: Dict[str, int],
-                   discount_funcs: list, min_value: int, predicate) -> float:
+                   discount_funcs: dict) -> float:
         """
         to_buy_books - dictionary from book type and book amount
         key of dict -- book type
         value of dict -- book amount
         discount_funcs - functions for calculate discount
-        min_value - value for return from loop
-        predicate - predicate for discount function
         """
         to_buy_books = deepcopy(to_buy_books)
         price = 0
-        while len(to_buy_books) > min_value:
-            for discount_func in discount_funcs:
-                to_buy_books, price = discount_func(to_buy_books, price, predicate)
+        for number, discount_func in discount_funcs.items():
+            while len(to_buy_books) >= number:
+                to_buy_books, price = discount_func(to_buy_books, price)
 
         count = sum(to_buy_books.values())
         return price + self.book_price * count
