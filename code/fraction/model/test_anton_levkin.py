8ac7f6a303ac6fb5281bc83e39e77a725d07881e
Antonii30 <xyyryyx@gmail.com>
2020-11-24 20:53:17 +0300

    Test

--- code/fraction/model/test_anton_levkin.py
+++ code/fraction/model/test_anton_levkin.py
@@ -5,10 +5,10 @@ from fraction.model.fraction import Fraction
 
 class TestFractionClass(unittest.TestCase):
     def test_can_create_str_from_fraction(self):
-        frac = Fraction(p=3, q=4)
-        self.assertEqual(str(frac), '3/4')
+        frac = Fraction(p=2, q=4)
+        self.assertEqual(str(frac), '2/4')
 
     def test_mul_fraction(self):
-        f1 = Fraction(1, 2)
-        f2 = Fraction(0, 3)
-        self.assertTrue((f1 * f2).is_equal(0, 1))
+        f0 = Fraction(1, 2)
+        f1 = Fraction(0, 3)
+        self.assertTrue((f0 * f2).is_equal(0, 1))
