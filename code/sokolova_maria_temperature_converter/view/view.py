6e1b91770f47712b5fe5c8eaec6cac7c7ed61243
Sokolova Maria <mariamosonova28@gmail.com>
2020-11-29 23:58:40 +0300

    Name of package renamed and viewmodel , viewmodel tests and view added

---
+++ code/sokolova_maria_temperature_converter/view/view.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+
+from sokolova_maria_temperature_converter.viewmodel.viewmodel import TemperatureConverterViewModel
+
+
+class GUIView:
+    CAST_TYPES = ['fahrenheit', 'kelvin', 'newton']
+    view_model = TemperatureConverterViewModel()
+
+    def convert(self):
+        self.view_model.set_input_value(self.text_value.get(1.0, tk.END))
+        self.view_model.set_cast_type(self.CAST_TYPES[self.cast_types.current()])
+        self.view_model.convert()
+        self.result_label.configure(text=self.view_model.get_output_value())
+        self.error_label.configure(text=self.view_model.get_error())
+
+    def __init__(self):
+        root = tk.Tk()
+        root.title("Temperature Converter")
+        frame = tk.Frame(root)
+        frame.pack(fill=tk.BOTH, expand=True)
+        frame.columnconfigure(1, weight=1)
+        frame.rowconfigure(1, weight=1)
+
+        self.celsius_label = tk.Label(self, text="celsius", fg='black', font="Arial 14")
+        self.celsius_label.pack()
+
+        self.text_value = tk.Text(self, height=1, width=40)
+        self.text_value.pack()
+
+        self.cast_types_label = tk.Label(self, text="to", fg='black', font="Arial 14")
+        self.cast_types_label.pack()
+
+        self.cast_types = ttk.Combobox(self, height=1, width=3, values=self.CAST_TYPES)
+        self.cast_types.current(0)
+        self.cast_types.pack()
+
+        self.convert = tk.Button(self, text="convert", width=15, height=1, font="Arial 14", bg="light blue", command=self.convert)
+        self.convert.pack()
+
+        self.result_label = tk.Label(self, text="", fg='black', font="Arial 14")
+        self.result_label.pack()
+
+        self.error_label = tk.Label(self, text="", fg='black', font="Arial 14")
+        self.error_label.pack()
---
+++ code/sokolova_maria_temperature_converter/viewmodel/viewmodel_test.py
@@ -0,0 +1,71 @@
+import unittest
+from sokolova_maria_temperature_converter.viewmodel.viewmodel import TemperatureConverterViewModel
+
+class MyTestCase(unittest.TestCase):
+    def test_create_view_model(self):
+        view_model = TemperatureConverterViewModel()
+        self.assertTrue(isinstance(view_model, TemperatureConverterViewModel))
+
+    def test_preset_cast_type(self):
+        view_model = TemperatureConverterViewModel()
+        self.assertEqual(view_model.cast_type, 'fahrenheit')
+
+    def test_input_value_setter(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value(15.8)
+        self.assertEqual(view_model.get_input_value(), 15.8)
+
+    def test_cast_type_setter(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_cast_type('newton')
+        self.assertEqual(view_model.get_cast_type(), 'newton')
+
+    def test_converting_case_fahrenheit(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value("-13,5")
+        view_model.convert()
+        self.assertAlmostEqual(view_model.get_output_value(), 7.7)
+
+    def test_converting_case_kelvin(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value("-13,5")
+        view_model.set_cast_type("kelvin")
+        view_model.convert()
+        self.assertAlmostEqual(view_model.get_output_value(),  259.65)
+
+    def test_converting_case_newton(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value("-13,5")
+        view_model.set_cast_type("newton")
+        view_model.convert()
+        self.assertAlmostEqual(view_model.get_output_value(),  -4.455)
+
+    def test_error_converting_case_NaN(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value("some not a number string")
+        view_model.convert()
+        self.assertEqual(view_model.get_error(), "The value is not numeric.\nEnter a numeric value.")
+
+    def test_output_converting_case_NaN(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value("some not a number string")
+        view_model.convert()
+        self.assertEqual(view_model.get_output_value(), '')
+
+    def test_clear_output(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value("-13,5")
+        view_model.set_cast_type("newton")
+        view_model.convert()
+        view_model.clear_output()
+        self.assertEqual(view_model.get_output_value(), '')
+
+    def test_clear_error(self):
+        view_model = TemperatureConverterViewModel()
+        view_model.set_input_value("some not a number string")
+        view_model.convert()
+        view_model.clear_error()
+        self.assertEqual(view_model.get_error(), '')
+
+
+
