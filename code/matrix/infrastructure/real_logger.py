3c0aedc3085564d45ec5200c62ed67abbac90623
Victoria <vikasannikova1998@gmail.com>
2020-12-08 13:00:45 +0300

    add path

--- code/matrix/infrastructure/real_logger.py
+++ code/matrix/infrastructure/real_logger.py
@@ -6,8 +6,8 @@ from matrix.infrastructure.ilogger import ILogger
 class RealLogger(ILogger):
     def __init__(self):
         super(RealLogger, self).__init__()
-        logging.basicConfig(filename='tmp/matrix.log', level=logging.INFO)
+        #logging.basicConfig(filename='../../tmp/matrix.log', level=logging.INFO)
 
     def append_message_to_logs_list(self, message):
         self.logs_list.append(message)
-        logging.info(message)
+        #logging.info(message)
