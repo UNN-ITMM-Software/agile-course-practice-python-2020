7e6db8b5a0dda4097de6541b69159661b8d0d326
kapkaevn <kapkaevn@gmail.com>
2020-12-09 22:28:12 +0300

    done mvvm

---
+++ code/complex_calc/viewmodel/complex_test_viewmodel.py
@@ -0,0 +1,157 @@
+import unittest
+
+from complex_calc.viewmodel.complex_viewmodel import ComplexNumViewModel
+
+
+class TestFractionCalculatorViewModel(unittest.TestCase):
+
+    def setUp(self):
+        self.view_model = ComplexNumViewModel()
+
+    def test_by_default_button_disabled(self):
+        self.assertEqual('disabled', self.view_model.get_button_convert_state())
+
+    def test_when_entered_both_complex_nums_button_enabled(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+2i')
+        self.assertNotEqual('disabled', self.view_model.get_button_convert_state())
+
+    def test_can_retrieve_complex_nums_text(self):
+        self.view_model.set_first_complex_num(' 1+1i')
+        self.view_model.set_second_complex_num('1+2i')
+        actual_c_n_1 = self.view_model.get_first_complex_num()
+        actual_c_n_2 = self.view_model.get_second_complex_num()
+
+        self.assertEqual('1+1i', actual_c_n_1)
+        self.assertEqual('1+2i', actual_c_n_2)
+
+    def test_when_entered_both_complex_nums_then_clear_one_button_disabled(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+2i')
+        self.view_model.set_first_complex_num('')
+        self.assertEqual('disabled', self.view_model.get_button_convert_state())
+
+    def test_when_entered_not_complex_num_button_disabled(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1a')
+        self.assertEqual('disabled', self.view_model.get_button_convert_state())
+
+    def test_can_add_11_and_12(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+2i')
+        self.view_model.click_convert()
+        self.assertEqual('2.0 + 3.0i', self.view_model.get_answer_text())
+
+    def test_can_add_11_and_1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1')
+        self.view_model.click_convert()
+        self.assertEqual('2.0 + 1.0i', self.view_model.get_answer_text())
+
+    def test_can_add_11_and_1_neg1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1-1i')
+        self.view_model.click_convert()
+        self.assertEqual('2.0 + 0.0i', self.view_model.get_answer_text())
+
+    def test_can_sub_11_and_12(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+2i')
+        self.view_model.set_operation('-')
+        self.view_model.click_convert()
+        self.assertEqual('0.0 - 1.0i', self.view_model.get_answer_text())
+
+    def test_can_sub_11_and_1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1')
+        self.view_model.set_operation('-')
+        self.view_model.click_convert()
+        self.assertEqual('0.0 + 1.0i', self.view_model.get_answer_text())
+
+    def test_can_sub_11_and_1_neg1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1-1i')
+        self.view_model.set_operation('-')
+        self.view_model.click_convert()
+        self.assertEqual('0.0 + 2.0i', self.view_model.get_answer_text())
+
+    def test_can_mul_11_and_12(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+2i')
+        self.view_model.set_operation('*')
+        self.view_model.click_convert()
+        self.assertEqual('-1.0 + 3.0i', self.view_model.get_answer_text())
+
+    def test_can_mul_11_and_1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1')
+        self.view_model.set_operation('*')
+        self.view_model.click_convert()
+        self.assertEqual('1.0 + 1.0i', self.view_model.get_answer_text())
+
+    def test_can_mul_11_and_1_neg1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1-1i')
+        self.view_model.set_operation('*')
+        self.view_model.click_convert()
+        self.assertEqual('2.0 + 0.0i', self.view_model.get_answer_text())
+
+    def test_div_11_and_12(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+2i')
+        self.view_model.set_operation('/')
+        self.view_model.click_convert()
+        str_c_n = ''.join(self.view_model.get_answer_text().split()).replace('i', 'j')
+        c_n = complex(str_c_n)
+        self.assertTrue(abs(c_n.real - 0.6) < 1e-10 and abs(c_n.imag + 0.2) < 1e-10)
+
+    def test_can_div_11_and_1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1')
+        self.view_model.set_operation('/')
+        self.view_model.click_convert()
+        self.assertEqual('1.0 + 1.0i', self.view_model.get_answer_text())
+
+    def test_can_div_11_and_1_neg1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1-1i')
+        self.view_model.set_operation('/')
+        self.view_model.click_convert()
+        str_c_n = ''.join(self.view_model.get_answer_text().split()).replace('i', 'j')
+        c_n = complex(str_c_n)
+        self.assertTrue(abs(c_n.real - 0.0) < 1e-10 and abs(c_n.imag - 1.0) < 1e-10)
+
+    def test_can_div_11_and_0(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('0')
+        self.view_model.set_operation('/')
+        self.view_model.click_convert()
+        self.assertEqual('Error! Cannot divide by zero!', self.view_model.get_answer_text())
+
+    def test_can_eq_11_and_11(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+1i')
+        self.view_model.set_operation('==')
+        self.view_model.click_convert()
+        self.assertEqual('True', self.view_model.get_answer_text())
+
+    def test_can_eq_11_and_1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1')
+        self.view_model.set_operation('==')
+        self.view_model.click_convert()
+        self.assertEqual('False', self.view_model.get_answer_text())
+
+    def test_can_not_eq_11_and_11(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1+1i')
+        self.view_model.set_operation('!=')
+        self.view_model.click_convert()
+        self.assertEqual('False', self.view_model.get_answer_text())
+
+    def test_can_not_eq_11_and_1(self):
+        self.view_model.set_first_complex_num('1+1i')
+        self.view_model.set_second_complex_num('1')
+        self.view_model.set_operation('!=')
+        self.view_model.click_convert()
+        self.assertEqual('True', self.view_model.get_answer_text())
---
+++ code/complex_calc/viewmodel/complex_viewmodel.py
@@ -0,0 +1,98 @@
+import re
+
+from complex_calc.model.complex_num import ComplexNum
+
+
+class ComplexNumViewModel:
+    def __init__(self):
+        self.answer_text = ''
+        self.set_btn_disabled()
+        self.first_complex_num = ''
+        self.second_complex_num = ''
+        self.operation = '+'
+        self.set_btn_disabled()
+        self.set_second_complex_num_text_enabled()
+
+    def get_button_convert_state(self):
+        return self.button_convert_state
+
+    def set_second_complex_num(self, value):
+        self.second_complex_num = value.strip()
+        self.validate_text()
+
+    def validate_text(self):
+        is_first_complex_num = self.is_complex(self.first_complex_num)
+        is_second_complex_num = self.is_complex(self.second_complex_num)
+        if is_first_complex_num and is_second_complex_num:
+            self.set_btn_enabled()
+        else:
+            self.set_btn_disabled()
+
+    def set_btn_enabled(self):
+        self.button_convert_state = 'normal'
+
+    def set_btn_disabled(self):
+        self.button_convert_state = 'disabled'
+
+    def set_second_complex_num_text_enabled(self):
+        self.second_fraction_text_state = 'normal'
+
+    def set_second_complex_num_text_disabled(self):
+        self.second_complex_num_text_state = 'disabled'
+
+    def get_second_complex_num(self):
+        return self.second_complex_num
+
+    def set_first_complex_num(self, value):
+        self.first_complex_num = value.strip()
+        self.validate_text()
+
+    def get_first_complex_num(self):
+        return self.first_complex_num
+
+    def is_complex(self, string):
+        p = re.compile('^\s*[+-]?((\d+)?[.,])?(\d*)?\s*[+-]\s*((\d+)?[.,])?(\d+)?[*_x]?[ij]\s*$')
+        if p.match(string):
+            return True
+        return False
+
+    def get_complex_num_as_list_from_string(self, string):
+        complex_string = ''.join(string.split()).replace('i', 'j')
+        try:
+            c_n = complex(complex_string)
+            return c_n
+        except:
+            self.answer_text = "WRONG COMPLEX FORMAT"
+
+    def click_convert(self):
+        try:
+            first_c_n = self.get_complex_num_as_list_from_string(self.first_complex_num)
+            first_complex_num = ComplexNum(first_c_n.real, first_c_n.imag)
+            second_c_n = self.get_complex_num_as_list_from_string(self.second_complex_num)
+            second_complex_num = ComplexNum(second_c_n.real, second_c_n.imag)
+            if self.operation == '+':
+                self.answer_text = str(first_complex_num + second_complex_num)
+            elif self.operation == '-':
+                self.answer_text = str(first_complex_num - second_complex_num)
+            elif self.operation == '*':
+                self.answer_text = str(first_complex_num * second_complex_num)
+            elif self.operation == '/':
+                try:
+                    self.answer_text = str(first_complex_num / second_complex_num)
+                except:
+                    result = 'Error! Cannot divide by zero!'
+                    self.answer_text = str(result)
+            elif self.operation == '==':
+                self.answer_text = str(first_complex_num == second_complex_num)
+            elif self.operation == '!=':
+                self.answer_text = str(first_complex_num != second_complex_num)
+            else:
+                self.answer_text = "!!!!"
+        except:
+            self.answer_text = "WRONG COMPLEX FORMAT"
+
+    def get_answer_text(self):
+        return self.answer_text
+
+    def set_operation(self, operation):
+        self.operation = operation
