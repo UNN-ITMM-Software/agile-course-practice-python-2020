368bb81c161972792b10e8988d229fec332a317d
AleksandrPanov <sasha1_1997@mail.ru>
2020-12-10 16:27:14 +0300

    fix flate8

--- code/lines_intersect/viewmodel/viewmodel.py
+++ code/lines_intersect/viewmodel/viewmodel.py
@@ -15,9 +15,9 @@ class LinesIntersectViewModel:
     @staticmethod
     def is_valid_coord(value):
         return re.fullmatch(LinesIntersectViewModel.VALID_COORD, value) is not None
-    
-    def set_line(val):
-        if LinesIntersectViewModel.is_valid_coord(val):
-            self.__line1 = val.split()[0]
-            self.__line2 = val.split()[1]
 
+    def set_line(self, val1, val2):
+        if LinesIntersectViewModel.is_valid_coord(val1):
+            self.__line1 = Line(val1.split()[0], val1.split()[1])
+        if LinesIntersectViewModel.is_valid_coord(val2):
+            self.__line1 = Line(val2.split()[0], val2.split()[1])
