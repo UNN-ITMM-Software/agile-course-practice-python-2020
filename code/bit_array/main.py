4469091bf7f9ebfd6edb6930128c8f4646b0fa0f
foxocat <dmitrichev.nick@gmail.com>
2020-12-11 19:51:57 +0300

    main done. as me

---
+++ code/bit_array/main.py
@@ -0,0 +1,4 @@
+from bit_array.view import view
+
+if __name__ == '__main__':
+    view.GUIView()
---
+++ code/bit_array/view/view.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import ttk
+from bit_array.viewmodel import viewmodel
+
+
+class GUIView(object):
+    def __init__(self):
+        self.bug = True
