41f864a1463328d6c6ad7515fb2d683b25c0f3cc
AleksandrPanov <sasha1_1997@mail.ru>
2020-12-12 19:49:12 +0300

    added get/set/calc tests

--- code/lines_intersect/viewmodel/viewmodel.py
+++ code/lines_intersect/viewmodel/viewmodel.py
@@ -4,13 +4,14 @@ from lines_intersect.model.lines_intersect import Line
 
 class LinesIntersectViewModel:
     VALID_COORD = r"([-]?\d+[\.]?\d*[ ]+[-]?\d+[\.]?\d*)"
+    RESULT_STR = "Intersection: %s"
 
     def __init__(self):
         self._point1 = ""
         self._point2 = ""
         self._point3 = ""
         self._point4 = ""
-        set_button_calculate_disabled()
+        self.set_button_calculate_disabled()
         self._result = ""
         
     def set_button_calculate_enabled(self):
@@ -21,21 +22,59 @@ class LinesIntersectViewModel:
 
     def get_button_calculate_state(self):
         return self._button_calculate_state
+        
+    def validate_text(self):
+        if self.is_valid_coord(self._point1) and \
+           self.is_valid_coord(self._point2) and \
+           self.is_valid_coord(self._point3) and \
+           self.is_valid_coord(self._point4):
+            self.set_button_calculate_enabled()
+        else:
+            self.set_button_calculate_disabled()
+        
+    def get_point1(self):
+        return self._point1
+        
+    def get_point2(self):
+        return self._point2
+    
+    def get_point3(self):
+        return self._point3
+    
+    def get_point4(self):
+        return self._point4
+        
+    def set_point1(self, value):
+        self._point1 = value
+        self.validate_text()
+        
+    def set_point2(self, value):
+        self._point2 = value
+        self.validate_text()
+        
+    def set_point3(self, value):
+        self._point3 = value
+        self.validate_text()
+        
+    def set_point4(self, value):
+        self._point4 = value
+        self.validate_text()
+        
+    def get_result(self):
+        return self._result
 
     @staticmethod
     def is_valid_coord(value):
         return re.fullmatch(LinesIntersectViewModel.VALID_COORD, value) is not None
 
-    def set_line(self, val1, val2):
-        if LinesIntersectViewModel.is_valid_coord(val1):
-            self._line1 = Line(val1.split()[0], val1.split()[1])
-        if LinesIntersectViewModel.is_valid_coord(val2):
-            self._line1 = Line(val2.split()[0], val2.split()[1])
-
-    def click_caclulate(self):
+    def click_calculate(self):
         if self.get_button_calculate_state() == "disabled":
             return
-        line1 = Line(self._point1.split(), self._point2.split())
-        line2 = Line(self._point3.split(), self._point4.split())
+            
+        def str_to_point(s):
+            return [float(el) for el in s.split()]
+            
+        line1 = Line(str_to_point(self._point1), str_to_point(self._point2))
+        line2 = Line(str_to_point(self._point3), str_to_point(self._point4))
 
-        self._result = str(Line.is_intersect(line1, line2))
+        self._result = self.RESULT_STR % (str(Line.is_intersect(line1, line2)))
