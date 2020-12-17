89d6ce76aa4acf250e9b36756c0fb49e2a98b2a3
Vladimir <folififo@gmail.com>
2020-12-17 15:05:54 +0300

    fix: fixed travis

--- code/deposit_calc/viewmodel/viewmodel.py
+++ code/deposit_calc/viewmodel/viewmodel.py
@@ -22,46 +22,28 @@ class DepositCalcViewModel:
         return self.handle_btn_state
 
     def set_start_depo(self, start_depo):
-        try:
-            self.start_depo = float(start_depo)
-            self.set_handle_btn_enabled()
-        except:
-            self.set_handle_btn_disabled()
+        self.start_depo = float(start_depo)
+        self.set_handle_btn_enabled()
 
     def set_depo_time(self, depo_time):
-        try:
-            self.depo_time = float(depo_time)
-            self.set_handle_btn_enabled()
-        except:
-            self.set_handle_btn_disabled()
+        self.depo_time = float(depo_time)
+        self.set_handle_btn_enabled()
 
     def set_rate(self, rate):
-        try:
-            self.rate = float(rate)
-            self.set_handle_btn_enabled()
-        except:
-            self.set_handle_btn_disabled()
+        self.rate = float(rate)
+        self.set_handle_btn_enabled()
 
     def set_capitalization(self, capitalization):
-        try:
-            self.capitalization = int(capitalization)
-            self.set_handle_btn_enabled()
-        except:
-            self.set_handle_btn_disabled()
+        self.capitalization = int(capitalization)
+        self.set_handle_btn_enabled()
 
     def set_replenishment_freq(self, replenishment_freq):
-        try:
-            self.replenishment_freq = int(replenishment_freq)
-            self.set_handle_btn_enabled()
-        except:
-            self.set_handle_btn_disabled()
+        self.replenishment_freq = int(replenishment_freq)
+        self.set_handle_btn_enabled()
 
     def set_replenishment_size(self, replenishment_size):
-        try:
-            self.replenishment_size = int(replenishment_size)
-            self.set_handle_btn_enabled()
-        except:
-            self.set_handle_btn_disabled()
+        self.replenishment_size = int(replenishment_size)
+        self.set_handle_btn_enabled()
 
     def get_start_depo(self):
         return self.start_depo
