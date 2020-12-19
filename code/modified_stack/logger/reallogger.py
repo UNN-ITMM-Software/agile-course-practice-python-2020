161e506a356426281d1e7ef7807c66314aaa32ba
VlaKuz <vvk-97@mail.ru>
2020-12-19 12:38:36 +0300

    fix log save

--- code/modified_stack/logger/reallogger.py
+++ code/modified_stack/logger/reallogger.py
@@ -3,18 +3,14 @@ import os
 
 from modified_stack.logger.ilogger import ILogger
 
-MAIN_FILEPATH = '../../tmp/modified_stack.log'
-TEST_FILEPATH = '../../../tmp/modified_stack.log'
-
+TEST_FILEPATH = '../../tmp/'
+LOG_FILEPATH = '../../tmp/modified_stack.log'
 
 class RealLogger(ILogger):
     def __init__(self):
         super(RealLogger, self).__init__()
-        if os.path.exists(MAIN_FILEPATH):
-            filepath = MAIN_FILEPATH
-        else:
-            filepath = TEST_FILEPATH
-        logging.basicConfig(filename=filepath, level=logging.INFO)
+        os.makedirs(TEST_FILEPATH, exist_ok=True)
+        logging.basicConfig(filename=LOG_FILEPATH, level=logging.INFO)
 
     def log(self, message):
         self.log_messages.append(message)
