fa65007a46fa5409f4b0235c49d6600f636d8810
VlaKuz <vvk-97@mail.ru>
2020-12-19 12:01:48 +0300

    fix log path to main and test

--- code/modified_stack/logger/reallogger.py
+++ code/modified_stack/logger/reallogger.py
@@ -1,12 +1,20 @@
 import logging
+import os
 
 from modified_stack.logger.ilogger import ILogger
+
+MAIN_FILEPATH = '../../tmp/modified_stack.log'
+TEST_FILEPATH = '../../../tmp/modified_stack.log'
 
 
 class RealLogger(ILogger):
     def __init__(self):
         super(RealLogger, self).__init__()
-        logging.basicConfig(filename='../../tmp/modified_stack.log', level=logging.INFO)
+        if os.path.exists(MAIN_FILEPATH):
+            filepath = MAIN_FILEPATH
+        else:
+            filepath = TEST_FILEPATH
+        logging.basicConfig(filename=filepath, level=logging.INFO)
 
     def log(self, message):
         self.log_messages.append(message)
