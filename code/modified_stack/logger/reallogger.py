7c2797f90e6157f2a8c96c82a9567431c0a1e94c
VlaKuz <vvk-97@mail.ru>
2020-12-19 11:16:35 +0300

    fix way log2

--- code/modified_stack/logger/reallogger.py
+++ code/modified_stack/logger/reallogger.py
@@ -6,7 +6,7 @@ from modified_stack.logger.ilogger import ILogger
 class RealLogger(ILogger):
     def __init__(self):
         super(RealLogger, self).__init__()
-        logging.basicConfig(filename='../../../tmp/modified_stack.log', level=logging.INFO)
+        logging.basicConfig(filename='../../tmp/modified_stack.log', level=logging.INFO)
 
     def log(self, message):
         self.log_messages.append(message)
