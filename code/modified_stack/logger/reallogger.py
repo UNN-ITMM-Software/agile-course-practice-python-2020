89c644c0704343e986710c1e6cf89dff4ea0af7e
VlaKuz <vvk-97@mail.ru>
2020-12-19 13:03:21 +0300

    fix way log

--- code/modified_stack/logger/reallogger.py
+++ code/modified_stack/logger/reallogger.py
@@ -1,17 +1,12 @@
 import logging
-import os
 
 from modified_stack.logger.ilogger import ILogger
-
-TEST_FILEPATH = '../../tmp/'
-LOG_FILEPATH = '../../tmp/modified_stack.log'
 
 
 class RealLogger(ILogger):
     def __init__(self):
         super(RealLogger, self).__init__()
-        os.makedirs(TEST_FILEPATH, exist_ok=True)
-        logging.basicConfig(filename=LOG_FILEPATH, level=logging.INFO)
+        logging.basicConfig(filename='../../tmp/modified_stack.log', level=logging.INFO)
 
     def log(self, message):
         self.log_messages.append(message)
