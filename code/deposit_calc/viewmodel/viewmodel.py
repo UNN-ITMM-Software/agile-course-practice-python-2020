e58a42074b407e95639f632bb67d4302b414c426
Krasikova Ekaterina <katrinkrasikova@mail.ru>
2020-12-20 21:22:23 +0300

    Add logging to viewmodel and tests for it

--- code/deposit_calc/viewmodel/viewmodel.py
+++ code/deposit_calc/viewmodel/viewmodel.py
@@ -1,8 +1,10 @@
 from deposit_calc.model.deposit_calc import DepositCalc
+from fraction.logger.fakelogger import FakeLogger
 
 
 class DepositCalcViewModel:
-    def __init__(self):
+    def __init__(self, logger=FakeLogger()):
+        self.logger = logger
         self.start_depo = 0.0
         self.depo_time = 0.0
         self.rate = 0.0
@@ -11,6 +13,7 @@ class DepositCalcViewModel:
         self.replenishment_size = 0.0
         self.result = ''
         self.set_handle_btn_disabled()
+        self.logger.log('Welcome!')
 
     def set_handle_btn_enabled(self):
         self.handle_btn_state = 'normal'
@@ -23,26 +26,32 @@ class DepositCalcViewModel:
 
     def set_start_depo(self, start_depo):
         self.start_depo = float(start_depo)
+        self.logger.log('Setting start deposit to %s' % self.start_depo)
         self.set_handle_btn_enabled()
 
     def set_depo_time(self, depo_time):
         self.depo_time = float(depo_time)
+        self.logger.log('Setting deposit time to %s' % self.depo_time)
         self.set_handle_btn_enabled()
 
     def set_rate(self, rate):
         self.rate = float(rate)
+        self.logger.log('Setting interest rate to %s' % self.rate)
         self.set_handle_btn_enabled()
 
     def set_capitalization(self, capitalization):
         self.capitalization = int(capitalization)
+        self.logger.log('Setting capitalization frequency to %s' % self.capitalization)
         self.set_handle_btn_enabled()
 
     def set_replenishment_freq(self, replenishment_freq):
         self.replenishment_freq = int(replenishment_freq)
+        self.logger.log('Setting replenishment frequency to %s' % self.replenishment_freq)
         self.set_handle_btn_enabled()
 
     def set_replenishment_size(self, replenishment_size):
         self.replenishment_size = int(replenishment_size)
+        self.logger.log('Setting replenishment size to %s' % self.replenishment_size)
         self.set_handle_btn_enabled()
 
     def get_start_depo(self):
@@ -52,6 +61,7 @@ class DepositCalcViewModel:
         return self.result
 
     def handle_btn_clicked(self):
+        self.logger.log('Button clicked')
         calc = DepositCalc(
             s0=self.start_depo,
             t=self.depo_time,
@@ -67,3 +77,5 @@ class DepositCalcViewModel:
             arr.append(self.replenishment_size)
 
         self.result = str(calc.revenue(values=arr))
+        self.logger.log('Result: %s' % self.result)
+
