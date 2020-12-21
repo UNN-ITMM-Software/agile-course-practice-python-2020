d497496f5e395478fa1ba316e8e58bc61541323a
ErmolaevMakcum <Ermolaev.Makcum@yandex.ru>
2020-12-21 23:05:40 +0300

    Some edits

--- code/statistics/logger/reallogger.py
+++ code/statistics/logger/reallogger.py
@@ -1,12 +1,15 @@
 import logging
+import os
 
 from statistics.logger.ilogger import ILogger
 
 
 class RealLogger(ILogger):
-    def __init__(self):
+    def __init__(self, log_file_path=None):
         super(RealLogger, self).__init__()
-        logging.basicConfig(filename='../../../tmp/statistics.log', level=logging.INFO)
+        if log_file_path is None:
+            log_file_path = os.path.join('..', '..', 'tmp', 'statistics.log')
+        logging.basicConfig(filename=log_file_path, level=logging.INFO)
 
     def log(self, message):
         self.log_messages.append(message)
