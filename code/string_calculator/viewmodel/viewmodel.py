7a3ccbea453b8dc3bb2e57e5fe119fd2c3af2184
Andrey Gluzdov <adgluzdoff@gmail.com>
2020-12-23 18:25:43 +0300

    TDD solved, code refactored and tests passed

--- code/string_calculator/viewmodel/viewmodel.py
+++ code/string_calculator/viewmodel/viewmodel.py
@@ -1,20 +1,28 @@
+import os
+
 from string_calculator.model.string_calculator import StrCalculator, StrCalculatorParser
+from string_calculator.logger.reallogger import RealLogger
 
 
 class StrCalculatorViewModel:
-    def __init__(self):
+    def __init__(self,
+                 logger=RealLogger(os.path.join('..', '..', 'tmp', 'numerical_integration.log'))):
+        self.logger = logger
+        self.logger.log('Hello')
         self.instr = ''
         self.answer = ''
+        self.button_convert_state = 'disabled'
         self.strcalc = StrCalculator()
-        self.set_btn_disabled()
 
     def get_button_convert_state(self):
         return self.button_convert_state
 
     def set_btn_enabled(self):
+        self.logger.log('Button status: normal')
         self.button_convert_state = 'normal'
 
     def set_btn_disabled(self):
+        self.logger.log('Button status: disabled')
         self.button_convert_state = 'disabled'
 
     def validate_text(self):
@@ -22,11 +30,14 @@ class StrCalculatorViewModel:
         try:
             parser.parse(self.instr)
         except:
+            self.logger.log('Invalid input')
             self.set_btn_disabled()
         else:
+            self.logger.log('Correct input')
             self.set_btn_enabled()
 
     def set_instr(self, instr):
+        self.logger.log('Set inst: {}'.format(instr))
         self.instr = instr
         self.validate_text()
 
@@ -35,9 +46,11 @@ class StrCalculatorViewModel:
 
     def set_answer(self, answer_str):
         self.answer = self.strcalc.add(self.instr)
+        self.logger.log('Answer: {}'.format(self.answer))
 
     def get_answer(self):
         return self.answer
 
     def click_button(self):
+        self.logger.log('Button clicked')
         self.set_answer(self.instr)
