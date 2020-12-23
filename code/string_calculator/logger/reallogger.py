cce2b5fa4c84d2053e8474024524836c4c221ec1
Andey Gluzdov <adgluzdoff@gmail.com>
2020-12-23 19:53:57 +0300

    Update reallogger.py
    fix2

--- code/string_calculator/logger/reallogger.py
+++ code/string_calculator/logger/reallogger.py
@@ -8,7 +8,7 @@ class RealLogger(ILogger):
     def __init__(self, log_file_path=None):
         super(RealLogger, self).__init__()
         if log_file_path is None:
-            log_file_path = os.path.join('..', '..', '..', 'tmp', 'numerical_integration.log')
+            log_file_path = os.path.join('..', '..', '..', 'tmp', 'string_calculator.log')
         logging.basicConfig(filename=log_file_path, level=logging.INFO)
 
     def log(self, message):
