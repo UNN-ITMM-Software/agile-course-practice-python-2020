fba23368f6481e64b6c18aa83f86922a59733653
Maria Protas <maria.protas@intel.com>
2020-12-24 21:38:03 +0300

    fix flake

--- code/fibonacci_heap/logger/test_fakelogger.py
+++ code/fibonacci_heap/logger/test_fakelogger.py
@@ -2,7 +2,14 @@ import unittest
 
 from fibonacci_heap.logger.fakelogger import FakeLogger as Logger
 
-test_log_msg = lambda msg_num: 'This is the [{}] test log msg'.format(msg_num)
+
+def test_log_msg(msg_index: int) -> str:
+    """
+    Get the test log msg and insert its number by order
+    :param msg_index: the id of test message
+    :return: the message
+    """
+    return 'This is the [{}] test log msg'.format(msg_index)
 
 
 class TestLogger(unittest.TestCase):
@@ -19,19 +26,19 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(log, [])
 
     def test_after_logging_message_in_log(self):
-        self.logger.log(test_log_msg(msg_num=1))
+        self.logger.log(test_log_msg(msg_index=1))
 
-        self.assertEqual([test_log_msg(msg_num=1)], self.logger.get_log_messages())
+        self.assertEqual([test_log_msg(msg_index=1)], self.logger.get_log_messages())
 
     def test_can_log_several_messages(self):
-        self.logger.log(test_log_msg(msg_num=1))
-        self.logger.log(test_log_msg(msg_num=2))
+        self.logger.log(test_log_msg(msg_index=1))
+        self.logger.log(test_log_msg(msg_index=2))
 
-        self.assertEqual([test_log_msg(msg_num=1), test_log_msg(msg_num=2)],
+        self.assertEqual([test_log_msg(msg_index=1), test_log_msg(msg_index=2)],
                          self.logger.get_log_messages())
 
     def test_can_get_last_log(self):
-        self.logger.log(test_log_msg(msg_num=1))
-        self.logger.log(test_log_msg(msg_num=2))
+        self.logger.log(test_log_msg(msg_index=1))
+        self.logger.log(test_log_msg(msg_index=2))
 
-        self.assertEqual(test_log_msg(msg_num=2), self.logger.get_last_message())
+        self.assertEqual(test_log_msg(msg_index=2), self.logger.get_last_message())
