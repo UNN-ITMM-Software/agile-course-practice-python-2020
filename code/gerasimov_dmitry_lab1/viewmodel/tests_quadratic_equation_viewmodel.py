e70cf88ad35005a42d90e42b926dce0d85e363cf
Liubov Batanina <piccione-mail@yandex.ru>
2020-12-25 10:07:59 +0300

    Added tests

--- code/gerasimov_dmitry_lab1/viewmodel/tests_quadratic_equation_viewmodel.py
+++ code/gerasimov_dmitry_lab1/viewmodel/tests_quadratic_equation_viewmodel.py
@@ -52,3 +52,53 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(1, model.get_a())
         self.assertEqual(10, model.get_b())
         self.assertEqual(-12, model.get_c())
+
+
+class TestViewModelLogger(unittest.TestCase):
+    def test_init_logging(self):
+        model = QuadraticEquationViewModel(2, 4, 2)
+        self.assertEqual('Welcome!', model.logger.get_last_message())
+
+    def test_set_a_logging(self):
+        model = QuadraticEquationViewModel()
+        model.set_a(2)
+        self.assertEqual('Setting a = 2', model.logger.get_last_message())
+
+    def test_set_b_logging(self):
+        model = QuadraticEquationViewModel()
+        model.set_b(2)
+        self.assertEqual('Setting b = 2', model.logger.get_last_message())
+
+    def test_set_c_logging(self):
+        model = QuadraticEquationViewModel()
+        model.set_c(2)
+        self.assertEqual('Setting c = 2', model.logger.get_last_message())
+
+    def test_model_perform_logging(self):
+        model = QuadraticEquationViewModel()
+        model.set_a(2)
+        model.set_b(4)
+        model.set_c(2)
+        model.perform()
+        self.assertEqual('Result = Answer:\n x = -1.0', model.logger.get_last_message())
+
+    def test_model_perform_all_message_logging(self):
+        model = QuadraticEquationViewModel()
+        model.set_a(2)
+        model.set_b(4)
+        model.set_c(2)
+        model.perform()
+
+        log_message = ['Setting a = 2', 'Setting b = 4', 'Setting c = 2',
+                       'Button clicked', 'a = 2, b = 4, c = 2', 'Result = Answer:\n x = -1.0']
+        self.assertEqual(log_message, model.logger.get_log_messages()[-6:])
+
+    def test_input_wrong_data(self):
+        model = QuadraticEquationViewModel()
+        model.set_a('@1')
+        model.set_b(4)
+        model.set_c(2)
+        model.perform()
+
+        wrong_message = 'Incorrect input'
+        self.assertEqual(wrong_message, model.logger.get_last_message())
