03154307110555d8bb81cae2f0fae1c11ad1cd69
Kirill Ryabinin <kir.riabinin@gmail.com>
2020-12-26 12:14:11 +0300

    Add init raises

--- code/ryabinin_kirill_lab1/src/elastic_of_demand.py
+++ code/ryabinin_kirill_lab1/src/elastic_of_demand.py
@@ -9,4 +9,7 @@ class InvalidDeltaPriceError(Exception):
 
 class ElasticOfDemand:
     def __init__(self, start_price, finish_price, start_demand, finish_demand):
-        pass
+        if finish_demand == 0:
+            raise InvalidDemandError('demand cannot be equal to zero')
+        if finish_price - start_price == 0:
+            raise InvalidDeltaPriceError('delta price cannot be equal to zero')
