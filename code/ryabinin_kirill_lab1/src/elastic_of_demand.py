cf7dae3a720694f59118d23b15b60fb60051513a
Kirill Ryabinin <kir.riabinin@gmail.com>
2020-12-26 11:55:19 +0300

    add class Elastic of demand

---
+++ code/ryabinin_kirill_lab1/src/elastic_of_demand.py
@@ -0,0 +1,12 @@
+
+class InvalidDemandError(Exception):
+    pass
+
+
+class InvalidDeltaPriceError(Exception):
+    pass
+
+
+class ElasticOfDemand:
+    def __init__(self, start_price, finish_price, start_demand, finish_demand):
+        pass
---
+++ code/ryabinin_kirill_lab1/src/test_elastic_of_demand.py
@@ -0,0 +1,14 @@
+import unittest
+
+from ryabinin_kirill_lab1.src.elastic_of_demand import ElasticOfDemand, InvalidDemandError, InvalidDeltaPriceError
+
+
+class TestElasticOfDemand(unittest.TestCase):
+
+    def test_raise_init_with_zero_demand(self):
+        with self.assertRaises(InvalidDemandError):
+            ElasticOfDemand(4, 3, 0, 2)
+
+    def test_raise_init_with_zero_delta_price(self):
+        with self.assertRaises(InvalidDeltaPriceError):
+            ElasticOfDemand(3, 3, 1, 2)
