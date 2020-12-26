f05497a8337b828b5e5c23ef11fc44f978f4ee89
AleksandrPanov <sasha1_1997@mail.ru>
2020-12-26 14:24:19 +0300

    added fakelogger_test

---
+++ code/hashmap/logger/test_fakelogger.py
@@ -0,0 +1,31 @@
+import unittest
+
+from hashmap.logger.fakelogger import FakeLogger as Logger
+
+
+class TestLogger(unittest.TestCase):
+    def setUp(self):
+        self.logger = Logger()
+
+    def test_can_create_logger(self):
+        self.assertTrue(isinstance(self.logger, Logger))
+
+    def test_default_log_is_empty(self):
+        log = self.logger.get_log_messages()
+        self.assertEqual(log, [])
+
+    def test_get_log_message(self):
+        self.logger.log('test_log1')
+        self.assertEqual(['test_log1'], self.logger.get_log_messages())
+
+    def test_get_log_messages(self):
+        self.logger.log('test_log1')
+        self.logger.log('test_log2')
+        self.logger.log('test_log3')
+        self.assertEqual(['test_log1', 'test_log2', 'test_log3'], self.logger.get_log_messages())
+
+    def test_get_last_message(self):
+        self.logger.log('test_log1')
+        self.logger.log('test_log2')
+        self.logger.log('test_log3')
+        self.assertEqual('test_log3', self.logger.get_last_message())
