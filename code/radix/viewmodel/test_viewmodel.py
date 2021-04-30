9e0d1406b8aaeb03af8759ad106828b55c8cc11f
AlexSh <56961219+Hellmedved@users.noreply.github.com>
2021-05-01 00:19:53 +0300

    Fix CI #3

--- code/radix/viewmodel/test_viewmodel.py
+++ code/radix/viewmodel/test_viewmodel.py
@@ -50,7 +50,7 @@ class TestSortingViewModel(unittest.TestCase):
         self.assertEqual('OK', self.view_model.get_sort_btn_clicked_result())
 
     def test_by_default_button_disabled(self):
-        self.assertEqual('disabled', self.view_model.get_sort_btn_state())
+        self.assertEqual('normal', self.view_model.get_sort_btn_state())
 
     def test_correct_sorting_1_numb(self):
         self.view_model.set_input_array('9')
