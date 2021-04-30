317e5a24bb30984f9fd3410eb6b8327901d73fe2
Alexander <demoii52@gmail.com>
2021-05-01 00:50:11 +0300

    Clean up the repo; Work Done

--- code/radix/viewmodel/viewmodel.py
+++ code/radix/viewmodel/viewmodel.py
@@ -48,12 +48,6 @@ class SortingViewModel:
     def get_sort_btn_clicked_result(self):
         return self.sort_btn_clicked
 
-#     def set_sort_btn_disabled(self):
-#        self.sort_btn_state = 'normal'
-
-#     def get_sort_btn_state(self):
-#        return self.sort_btn_state
-
     def sort_btn_clicked(self):
         try:
             self.logger.log('Start reading the array')
