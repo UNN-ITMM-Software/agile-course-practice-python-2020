e5608a1abb56e180428f999d222e2ffd3e50f5b8
petrshkenev <potc.coas@live.ru>
2021-05-20 05:53:59 +0300

    Покрытие тестами

---
+++ code/shkenev_petr/logger/test_ilogger.py
@@ -0,0 +1,20 @@
+import unittest
+
+from .ilogger import ILogger
+
+
+class TestILogger(unittest.TestCase):
+
+    def test_create(self):
+        with self.assertRaises(NotImplementedError):
+                logger = ILogger()
+
+    def test_get_messages_list(self):
+        with self.assertRaises(NotImplementedError):
+                logger = ILogger()
+                logger.get_messages_list()
+
+    def test_log(self):
+        with self.assertRaises(NotImplementedError):
+                logger = ILogger()
+                logger.log()
